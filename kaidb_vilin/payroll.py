8dbc3efcb5bfebbf578982630708782de0c268f9
Kai Bernardini <kbernardini@Kais-MacBook-Pro.local>
2018-02-12 16:46:07 -0500

    added prov

--- kaidb_vilin/payroll.py
+++ kaidb_vilin/payroll.py
@@ -64,7 +64,7 @@ class payroll(dml.Algorithm):
 
         # Obj transformation: 
         #  df --> string (json formated) --> json 
-        r = json.loads(data.to_json( orient='index'))
+        r = json.loads(data.to_json( orient='records'))
         # dump json. 
         # Keys should already be sorted
 
@@ -73,11 +73,11 @@ class payroll(dml.Algorithm):
         repo.dropCollection("payroll")
         repo.createCollection("payroll")
 
-        if payroll.DEBUG:
-            print(type(r))
-            assert isinstance(r, dict)
+        #if payroll.DEBUG:
+         #   print(type(r))
+          #  assert isinstance(r, dict)
 
-        repo['kaidb_vilin.payroll'].insert_many( [dict(r)])
+        repo['kaidb_vilin.payroll'].insert_many( r)
 
         repo['kaidb_vilin.payroll'].metadata({'complete':True})
         print(repo['kaidb_vilin.payroll'].metadata())
@@ -99,48 +99,39 @@ class payroll(dml.Algorithm):
         # Set up the database connection.
         client = dml.pymongo.MongoClient()
         repo = client.repo
-        repo.authenticate(contributor, contributor)
+        repo.authenticate('kaidb_vilin', 'kaidb_vilin')
         doc.add_namespace('alg', 'http://datamechanics.io/algorithm/') # The scripts are in <folder>#<filename> format.
         doc.add_namespace('dat', 'http://datamechanics.io/data/') # The data sets are in <user>#<collection> format.
         doc.add_namespace('ont', 'http://datamechanics.io/ontology#') # 'Extension', 'DataResource', 'DataSet', 'Retrieval', 'Query', or 'Computation'.
         doc.add_namespace('log', 'http://datamechanics.io/log/') # The event log.
-        doc.add_namespace('bdp', 'https://data.cityofboston.gov/resource/')
+        doc.add_namespace('dbd', 'https://data.boston.gov/dataset/')
 
-        this_script = doc.agent('alg:alice_bob#example', {prov.model.PROV_TYPE:prov.model.PROV['SoftwareAgent'], 'ont:Extension':'py'})
-        resource = doc.entity('bdp:wc8w-nujj', {'prov:label':'311, Service Requests', prov.model.PROV_TYPE:'ont:DataResource', 'ont:Extension':'json'})
-        get_found = doc.activity('log:uuid'+str(uuid.uuid4()), startTime, endTime)
-        get_lost = doc.activity('log:uuid'+str(uuid.uuid4()), startTime, endTime)
-        doc.wasAssociatedWith(get_found, this_script)
-        doc.wasAssociatedWith(get_lost, this_script)
-        doc.usage(get_found, resource, startTime, None,
+        this_script = doc.agent('alg:kaidb_vilin#payroll', {prov.model.PROV_TYPE:prov.model.PROV['SoftwareAgent'], 'ont:Extension':'py'})
+
+        resource = doc.entity('dbd:wc8w-nujj', {'prov:label':'employee-earnings-report-2016', prov.model.PROV_TYPE:'ont:DataResource', 'ont:Extension':'csv'})
+        get_payroll = doc.activity('log:uuid'+str(uuid.uuid4()), startTime, endTime)
+        doc.wasAssociatedWith(get_payroll, this_script)
+        
+        # How to retrieve the .csv
+        doc.usage(get_payroll, resource, startTime, None,
                   {prov.model.PROV_TYPE:'ont:Retrieval',
-                  'ont:Query':'?type=Animal+Found&$select=type,latitude,longitude,OPEN_DT'
+                  'ont:Query':'418983dc-7cae-42bb-88e4-d56f5adcf869/resource/8368bd3d-3633-4927-8355-2a2f9811ab4f/download/employee-earnings-report-2016.csv'
                   }
                   )
-        doc.usage(get_lost, resource, startTime, None,
-                  {prov.model.PROV_TYPE:'ont:Retrieval',
-                  'ont:Query':'?type=Animal+Lost&$select=type,latitude,longitude,OPEN_DT'
-                  }
-                  )
+     
 
-        lost = doc.entity('dat:alice_bob#lost', {prov.model.PROV_LABEL:'Animals Lost', prov.model.PROV_TYPE:'ont:DataSet'})
-        doc.wasAttributedTo(lost, this_script)
-        doc.wasGeneratedBy(lost, get_lost, endTime)
-        doc.wasDerivedFrom(lost, resource, get_lost, get_lost, get_lost)
-
-        found = doc.entity('dat:alice_bob#found', {prov.model.PROV_LABEL:'Animals Found', prov.model.PROV_TYPE:'ont:DataSet'})
-        doc.wasAttributedTo(found, this_script)
-        doc.wasGeneratedBy(found, get_found, endTime)
-        doc.wasDerivedFrom(found, resource, get_found, get_found, get_found)
-
+        payroll = doc.entity('dat:kaidb_vilin#payroll', {prov.model.PROV_LABEL:'Payroll', prov.model.PROV_TYPE:'ont:DataSet'})
+        doc.wasAttributedTo(payroll, this_script)
+        doc.wasGeneratedBy(payroll, get_payroll, endTime)
+        doc.wasDerivedFrom(payroll, resource, get_payroll, get_payroll, get_payroll)
         repo.logout()
                   
         return doc
 
 # comment this out for submission. 
 payroll.execute()
-#doc = example.provenance()
-#print(doc.get_provn())
-#print(json.dumps(json.loads(doc.serialize()), indent=4))
+doc = payroll.provenance()
+print(doc.get_provn())
+print(json.dumps(json.loads(doc.serialize()), indent=4))
 
 ## eof

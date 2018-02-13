4c971cfcabe031e54c0f4fe5064ab3e95941104e
lliu42 <32715725+lliu42@users.noreply.github.com>
2018-02-13 13:06:15 -0500

    Add files via upload

--- lliu_saragl/routes.py
+++ lliu_saragl/routes.py
@@ -5,10 +5,10 @@ import prov.model
 import datetime
 import uuid
 
-class parking(dml.Algorithm):
+class routes(dml.Algorithm):
     contributor = 'lliu_saragl'
     reads = []
-    writes = ['lliu_saragl.parking']
+    writes = ['lliu_saragl.routes']
 
     @staticmethod
     def execute(trial = False):
@@ -20,18 +20,17 @@ class parking(dml.Algorithm):
         repo = client.repo
         repo.authenticate('lliu_saragl', 'lliu_saragl')
 
-        url = 'http://bostonopendata-boston.opendata.arcgis.com/datasets/53ebc23fcc654111b642f70e61c63852_0.geojson'
+        url = 'http://bostonopendata-boston.opendata.arcgis.com/datasets/4f3e4492e36f4907bcd307b131afe4a5_0.geojson'
         response = urllib.request.urlopen(url).read().decode("utf-8")
         r = json.loads(response)
-        #print(r)
         s = json.dumps(r, sort_keys=True, indent=2)
         print(s)
-        repo.dropPermanent("parking")
-        repo.createPermanent("parking")
-        repo['lliu_saragl.parking'].insert_many(r['features'])
-        repo['lliu_saragl.violations'].metadata({'complete':True})
-        print(repo['lliu_saragl.violations'].metadata())
-
+        repo.dropPermanent("routes")
+        repo.createPermanent("routes")
+        
+        repo['lliu_saragl.routes'].insert_many(r['features'])
+        repo['lliu_saragl.routes'].metadata({'complete':True})
+        print(repo['lliu_saragl.routes'].metadata())
         
         repo.logout()
 
@@ -42,7 +41,7 @@ class parking(dml.Algorithm):
     @staticmethod
     def provenance(doc = prov.model.ProvDocument(), startTime = None, endTime = None):
         '''
-        Create the provenance document describing everything happening in this script.
+        Create the provenacne document describing everything happening in this script.
         Each run of the script will generate a new document describing that invocation event.
         '''
 
@@ -50,31 +49,30 @@ class parking(dml.Algorithm):
         client = dml.pymongo.MongoClient()
         repo = client.repo
         repo.authenticate('lliu_saragl', 'lliu_saragl')
-        
         doc.add_namespace('alg', 'http://datamechanics.io/algorithm/') # The scripts are in <folder>#<filename> format.
         doc.add_namespace('dat', 'http://datamechanics.io/data/') # The data sets are in <user>#<collection> format.
         doc.add_namespace('ont', 'http://datamechanics.io/ontology#') # 'Extension', 'DataResource', 'DataSet', 'Retrieval', 'Query', or 'Computation'.
         doc.add_namespace('log', 'http://datamechanics.io/log/') # The event log.
         doc.add_namespace('bdp', 'https://data.cityofboston.gov/resource/')
-        doc.add_namespace('bos', 'http://bostonopendata.boston.opendata.arcgis.com/') # Parking data
+        doc.add_namespace('bos', 'http://bostonopendata.boston.opendata.arcgis.com/')
 
-        this_script = doc.agent('alg:lliu_saragl#parking', {prov.model.PROV_TYPE:prov.model.PROV['SoftwareAgent'], 'ont:Extension':'py'})
-        resource = doc.entity('bos:53ebc23fcc654111b642f70e61c63852_0', {'prov:label':'Snow Emergency Parking', prov.model.PROV_TYPE:'ont:DataSet', 'ont:Extension':'json'})
-        get_parking = doc.activity('log:uuid'+str(uuid.uuid4()), startTime, endTime)
-        doc.wasAssociatedWith(get_parking, this_script)
-        doc.usage(get_parking, resource, startTime, None, {prov.model.PROV_TYPE:'ont:Retrieval'})
+        this_script = doc.agent('alg:lliu_saragl#routes', {prov.model.PROV_TYPE:prov.model.PROV['SoftwareAgent'], 'ont:Extension':'py'})
+        resource = doc.entity('bos:4f3e4492e36f4907bcd307b131afe4a5_0', {'prov:label': 'Snow Emergency Routes', prov.model.PROV_TYPE:'ont:DataSet', 'ont:Extension':'json'})
+        get_routes = doc.activity('log:uuid'+str(uuid.uuid4()), startTime, endTime)
+        doc.wasAssociatedWith(get_routes, this_script)
         
-        park = doc.entity('dat:lliu_saragl#parking',{prov.model.PROV_LABEL:'Snow Emergency Parking', prov.model.PROV_TYPE:'ont:DataSet'})
-        doc.wasAttributedTo(park, this_script)
-        doc.wasGeneratedBy(park, get_parking, endTime)
-        doc.wasDerivedFrom(park, get_parking, get_parking, get_parking)
+        doc.usage(get_routes, resource, startTime, None, {prov.model.PROV_TYPE:'ont:Retrieval'})
+        
+        routes = doc.entity('dat:lliu_saragl#routes',{prov.model.PROV_LABEL:'Snow Emergency Routes', prov.model.PROV_TYPE:'ont:DataSet'})
+        doc.wasAttributedTo(routes, this_script)
+        doc.wasGeneratedBy(routes, get_routes, endTime)
+        doc.wasDerivedFrom(routes, get_routes, get_routes, get_routes)
 
-        
         repo.logout()
 
         return doc
 
-parking.execute()
-doc = parking.provenance()
+routes.execute()
+doc = routes.provenance()
 print(doc.get_provn())
 print(json.dumps(json.loads(doc.serialize()), indent=4))
--- lliu_saragl/violations.py
+++ lliu_saragl/violations.py
@@ -5,10 +5,10 @@ import prov.model
 import datetime
 import uuid
 
-class parking(dml.Algorithm):
+class violations(dml.Algorithm):
     contributor = 'lliu_saragl'
     reads = []
-    writes = ['lliu_saragl.parking']
+    writes = ['lliu_saragl.violations']
 
     @staticmethod
     def execute(trial = False):
@@ -20,18 +20,17 @@ class parking(dml.Algorithm):
         repo = client.repo
         repo.authenticate('lliu_saragl', 'lliu_saragl')
 
-        url = 'http://bostonopendata-boston.opendata.arcgis.com/datasets/53ebc23fcc654111b642f70e61c63852_0.geojson'
+        url = 'https://data.boston.gov/api/3/action/datastore_search?resource_id=90ed3816-5e70-443c-803d-9a71f44470be&q=snow'
         response = urllib.request.urlopen(url).read().decode("utf-8")
         r = json.loads(response)
-        #print(r)
         s = json.dumps(r, sort_keys=True, indent=2)
         print(s)
-        repo.dropPermanent("parking")
-        repo.createPermanent("parking")
-        repo['lliu_saragl.parking'].insert_many(r['features'])
+        repo.dropPermanent("violations")
+        repo.createPermanent("violations")
+        
+        repo['lliu_saragl.violations'].insert_many(r['result']['records'])
         repo['lliu_saragl.violations'].metadata({'complete':True})
         print(repo['lliu_saragl.violations'].metadata())
-
         
         repo.logout()
 
@@ -42,7 +41,7 @@ class parking(dml.Algorithm):
     @staticmethod
     def provenance(doc = prov.model.ProvDocument(), startTime = None, endTime = None):
         '''
-        Create the provenance document describing everything happening in this script.
+        Create the provenacne document describing everything happening in this script.
         Each run of the script will generate a new document describing that invocation event.
         '''
 
@@ -50,31 +49,29 @@ class parking(dml.Algorithm):
         client = dml.pymongo.MongoClient()
         repo = client.repo
         repo.authenticate('lliu_saragl', 'lliu_saragl')
-        
         doc.add_namespace('alg', 'http://datamechanics.io/algorithm/') # The scripts are in <folder>#<filename> format.
         doc.add_namespace('dat', 'http://datamechanics.io/data/') # The data sets are in <user>#<collection> format.
         doc.add_namespace('ont', 'http://datamechanics.io/ontology#') # 'Extension', 'DataResource', 'DataSet', 'Retrieval', 'Query', or 'Computation'.
         doc.add_namespace('log', 'http://datamechanics.io/log/') # The event log.
         doc.add_namespace('bdp', 'https://data.cityofboston.gov/resource/')
-        doc.add_namespace('bos', 'http://bostonopendata.boston.opendata.arcgis.com/') # Parking data
 
-        this_script = doc.agent('alg:lliu_saragl#parking', {prov.model.PROV_TYPE:prov.model.PROV['SoftwareAgent'], 'ont:Extension':'py'})
-        resource = doc.entity('bos:53ebc23fcc654111b642f70e61c63852_0', {'prov:label':'Snow Emergency Parking', prov.model.PROV_TYPE:'ont:DataSet', 'ont:Extension':'json'})
-        get_parking = doc.activity('log:uuid'+str(uuid.uuid4()), startTime, endTime)
-        doc.wasAssociatedWith(get_parking, this_script)
-        doc.usage(get_parking, resource, startTime, None, {prov.model.PROV_TYPE:'ont:Retrieval'})
+        this_script = doc.agent('alg:lliu_saragl#violations', {prov.model.PROV_TYPE:prov.model.PROV['SoftwareAgent'], 'ont:Extension':'py'})
+        resource = doc.entity('bdp:90ed3816-5e70-443c-803d-9a71f44470be', {'prov:label': 'List of Snow Violations', prov.model.PROV_TYPE:'ont:DataResource', 'ont:Extension':'json'})
+        get_violations = doc.activity('log:uuid'+str(uuid.uuid4()), startTime, endTime)
+        doc.wasAssociatedWith(get_violations, this_script)
         
-        park = doc.entity('dat:lliu_saragl#parking',{prov.model.PROV_LABEL:'Snow Emergency Parking', prov.model.PROV_TYPE:'ont:DataSet'})
-        doc.wasAttributedTo(park, this_script)
-        doc.wasGeneratedBy(park, get_parking, endTime)
-        doc.wasDerivedFrom(park, get_parking, get_parking, get_parking)
+        doc.usage(get_violations, resource, startTime, None, {prov.model.PROV_TYPE:'ont:Retrieval'})
+        
+        violations = doc.entity('dat:lliu_saragl#violations',{prov.model.PROV_LABEL:'List of Snow Violations', prov.model.PROV_TYPE:'ont:DataSet'})
+        doc.wasAttributedTo(violations, this_script)
+        doc.wasGeneratedBy(violations, get_violations, endTime)
+        doc.wasDerivedFrom(violations, get_violations, get_violations, get_violations)
 
-        
         repo.logout()
 
         return doc
 
-parking.execute()
-doc = parking.provenance()
+violations.execute()
+doc = violations.provenance()
 print(doc.get_provn())
 print(json.dumps(json.loads(doc.serialize()), indent=4))

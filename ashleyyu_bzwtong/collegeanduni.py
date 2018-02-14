e968dc61c20ab9e128b418fc90fd7b0b4402cbb0
bzwtong <32522786+bzwtong@users.noreply.github.com>
2018-02-14 18:59:57 -0500

    Update collegeanduni.py

--- ashleyyu_bzwtong/collegeanduni.py
+++ ashleyyu_bzwtong/collegeanduni.py
@@ -52,6 +52,7 @@ class collegeanduni(dml.Algorithm):
         doc.add_namespace('dat', 'http://datamechanics.io/data/ashleyyu_bzwtong')  # The data sets are in <user>#<collection> format.
         doc.add_namespace('ont', 'http://datamechanics.io/ontology#')  # 'Extension', 'DataResource', 'DataSet', 'Retrieval', 'Query', or 'Computation'.
         doc.add_namespace('log', 'http://datamechanics.io/log/')  # The event log.
+        doc.add_namespace('bdp', 'https://data.cityofboston.gov/resource/')
 
         this_script = doc.agent('alg:ashleyyu_bzwtong#schools', {prov.model.PROV_TYPE: prov.model.PROV['SoftwareAgent'], 'ont:Extension': 'py'})
         resource = doc.entity('dat:College and Uni in Boston',

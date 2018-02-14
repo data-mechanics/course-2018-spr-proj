22f645c7d53712fe324007e76a1759342273c19d
Janell Chen <janellc@bu.edu>
2018-02-13 22:28:46 -0500

    changed a few provenance lines

--- janellc_rstiffel/getOpenSpace.py
+++ janellc_rstiffel/getOpenSpace.py
@@ -86,17 +86,20 @@ class getOpenSpace(dml.Algorithm):
         client = dml.pymongo.MongoClient()
         repo = client.repo
         repo.authenticate('janellc_rstiffel', 'janellc_rstiffel')
-        doc.add_namespace('alg', 'http://datamechanics.io/algorithm/') # The scripts are in <folder>#<filename> format.
-        doc.add_namespace('dat', 'http://datamechanics.io/data/') # The data sets are in <user>#<collection> format.
+        doc.add_namespace('alg', 'http://datamechanics.io/algorithm/janellc_rstiffel/') # The scripts are in <folder>#<filename> format.
+        doc.add_namespace('dat', 'http://datamechanics.io/data/janellc_rstiffel/') # The data sets are in <user>#<collection> format.
         doc.add_namespace('ont', 'http://datamechanics.io/ontology#') # 'Extension', 'DataResource', 'DataSet', 'Retrieval', 'Query', or 'Computation'.
         doc.add_namespace('log', 'http://datamechanics.io/log/') # The event log.
+        
         # Resource:
         doc.add_namespace('bod', 'http://bostonopendata-boston.opendata.arcgis.com/datasets/')
-
-
+        doc.add_namespace('zip', 'http://datamechanics.io/data/') 
+        
         # Agent, entity, activity of open Space
         this_script = doc.agent('alg:janellc_rstiffel#getOpenSpace', {prov.model.PROV_TYPE:prov.model.PROV['SoftwareAgent'], 'ont:Extension':'py'})
+        
         resource = doc.entity('bod:2868d370c55d4d458d4ae2224ef8cddd_7', {'prov:label':'311, Service Requests', prov.model.PROV_TYPE:'ont:DataResource', 'ont:Extension':'geojson'})
+        resource2 = doc.entity('zip:BostonZip', {'prov:label':'311, Service Requests', prov.model.PROV_TYPE:'ont:DataResource', 'ont:Extension':'csv'})
         
         get_openSpace = doc.activity('log:uuid'+str(uuid.uuid4()), startTime, endTime)
         get_zipCodes = doc.activity('log:uuid'+str(uuid.uuid4()), startTime, endTime)
@@ -109,7 +112,7 @@ class getOpenSpace(dml.Algorithm):
                   'ont:Query':''
                   }
                   )
-        doc.usage(get_zipCodes, resource, startTime, None,
+        doc.usage(get_zipCodes, resource2, startTime, None,
                   {prov.model.PROV_TYPE:'ont:Retrieval',
                   'ont:Query':''
                   }
@@ -123,7 +126,7 @@ class getOpenSpace(dml.Algorithm):
         zipCodes = doc.entity('dat:janellc_rstiffel#zipCodes', {prov.model.PROV_LABEL:'Boston Zip Codes', prov.model.PROV_TYPE:'ont:DataSet'})
         doc.wasAttributedTo(zipCodes, this_script)
         doc.wasGeneratedBy(zipCodes, get_zipCodes, endTime)
-        doc.wasDerivedFrom(zipCodes, resource, get_zipCodes, get_zipCodes, get_zipCodes)
+        doc.wasDerivedFrom(zipCodes, resource2, get_zipCodes, get_zipCodes, get_zipCodes)
 
         repo.logout()
                   

8e89057db4b19ffa9fbab6bf9f313d253f43796b
bzwtong <32522786+bzwtong@users.noreply.github.com>
2018-02-13 19:43:46 -0500

    Update and rename parkingdata.py to collegeanduni.py

--- ashleyyu_bzwtong/collegeanduni.py
+++ ashleyyu_bzwtong/collegeanduni.py
@@ -6,10 +6,10 @@ import datetime
 import uuid
 
 
-class parkingData(dml.Algorithm):
+class collegeanduni(dml.Algorithm):
     contributor = 'ashleyyu_bzwtong'
     reads = []
-    writes = ['ashleyyu_bzwtong.parkingData']
+    writes = ['ashleyyu_bzwtong.collegeanduni']
 
     @staticmethod
     def execute(trial=False):
@@ -23,12 +23,12 @@ class parkingData(dml.Algorithm):
 
         url = 'https://data.boston.gov/api/3/action/datastore_search?resource_id=8d38cc9d-8c58-462e-b2df-b793e9c05612&limit=572'
         response = urllib.request.urlopen(url).read().decode("utf-8")
-        parking_json = [json.loads(response)]
-        repo.dropCollection("parkingData")
-        repo.createCollection("parkingData")
-        repo['ashleyyu_bzwtong.parkingData'].insert_many(parking_json)
-        repo['ashleyyu_bzwtong.parkingData'].metadata({'complete': True})
-        print(repo['ashleyyu_bzwtong.parkingData'].metadata())
+        schools_json = [json.loads(response)]
+        repo.dropCollection("collegeanduni")
+        repo.createCollection("collegeanduni")
+        repo['ashleyyu_bzwtong.collegeanduni'].insert_many(schools_json)
+        repo['ashleyyu_bzwtong.collegeanduni'].metadata({'complete': True})
+        print(repo['ashleyyu_bzwtong.collegeanduni'].metadata())
 
         repo.logout()
 
@@ -48,34 +48,34 @@ class parkingData(dml.Algorithm):
         client = dml.pymongo.MongoClient()
         repo = client.repo
         repo.authenticate('ashleyyu_bzwtong', 'ashleyyu_bzwtong')
-        doc.add_namespace('alg', 'http://datamechanics.io/algorithm/aoconno8_dmak1112')  # The scripts are in <folder>#<filename> format.
-        doc.add_namespace('dat', 'http://datamechanics.io/data/aoconno8_dmak1112')  # The data sets are in <user>#<collection> format.
+        doc.add_namespace('alg', 'http://datamechanics.io/algorithm/ashleyyu_bzwtong')  # The scripts are in <folder>#<filename> format.
+        doc.add_namespace('dat', 'http://datamechanics.io/data/ashleyyu_bzwtong')  # The data sets are in <user>#<collection> format.
         doc.add_namespace('ont', 'http://datamechanics.io/ontology#')  # 'Extension', 'DataResource', 'DataSet', 'Retrieval', 'Query', or 'Computation'.
         doc.add_namespace('log', 'http://datamechanics.io/log/')  # The event log.
 
-        this_script = doc.agent('alg:ashleyyu_bzwtong#parkingData', {prov.model.PROV_TYPE: prov.model.PROV['SoftwareAgent'], 'ont:Extension': 'py'})
-        resource = doc.entity('dat:Cars Parked Across All Zones - 2015',
-                              {'prov:label': '2015 Parking Data', prov.model.PROV_TYPE: 'ont:DataResource',
+        this_script = doc.agent('alg:ashleyyu_bzwtong#schools', {prov.model.PROV_TYPE: prov.model.PROV['SoftwareAgent'], 'ont:Extension': 'py'})
+        resource = doc.entity('dat:College and Uni in Boston',
+                              {'prov:label': 'College and Uni Data', prov.model.PROV_TYPE: 'ont:DataResource',
                                'ont:Extension': 'csv'})
-        get_parkingData = doc.activity('log:uuid' + str(uuid.uuid4()), startTime, endTime)
-        doc.wasAssociatedWith(get_parkingData, this_script)
-        doc.usage(get_parkingData, resource, startTime, None,
+        get_schools = doc.activity('log:uuid' + str(uuid.uuid4()), startTime, endTime)
+        doc.wasAssociatedWith(get_schools, this_script)
+        doc.usage(get_schools, resource, startTime, None,
                   {prov.model.PROV_TYPE: 'ont:Retrieval'
                    }
                   )
-        parkingData = doc.entity('dat:ashleyyu_bzwtong#parkingData',
-                                   {prov.model.PROV_LABEL: 'Parking Data', prov.model.PROV_TYPE: 'ont:DataSet'})
-        doc.wasAttributedTo(parkingData, this_script)
-        doc.wasGeneratedBy(parkingData, get_parkingData, endTime)
-        doc.wasDerivedFrom(parkingData, resource, get_parkingData, get_parkingData, get_parkingData)
+        collegeanduni = doc.entity('dat:ashleyyu_bzwtong#schools',
+                                   {prov.model.PROV_LABEL: 'College and Uni', prov.model.PROV_TYPE: 'ont:DataSet'})
+        doc.wasAttributedTo(collegeanduni, this_script)
+        doc.wasGeneratedBy(collegeanduni, get_schools, endTime)
+        doc.wasDerivedFrom(collegeanduni, resource, get_schools, get_schools, get_schools)
 
         repo.logout()
 
         return doc
 
 
-parkingData.execute()
-doc = parkingData.provenance()
+collegeanduni.execute()
+doc = collegeanduni.provenance()
 print(doc.get_provn())
 print(json.dumps(json.loads(doc.serialize()), indent=4))
 

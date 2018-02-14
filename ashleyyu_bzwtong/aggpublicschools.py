0e548af8e93bc08bf455feee595eba1d3fdd4108
bzwtong <32522786+bzwtong@users.noreply.github.com>
2018-02-14 17:56:37 -0500

    Update aggpublicschools.py

--- ashleyyu_bzwtong/aggpublicschools.py
+++ ashleyyu_bzwtong/aggpublicschools.py
@@ -25,24 +25,22 @@ class aggpublicschools(dml.Algorithm):
         repo.dropPermanent("aggpublicschools")
         repo.createPermanent("aggpublicschools")
 
-        publicschools = list(repo.ashleyyu_bzwtong.publicschools.find())
-        
         zipCount= []
-        for entry in publicschools:
-            if "zipcode" in entry:
-                zipcd = entry["zipcode"]
-                zipCount += [(zipcd, 1)]
+        for entry in repo.ashleyyu_bzwtong.schools.find():
+            if "location_zip" in entry:
+                zipcode = entry["location_zip"]
+                zipCount += [(zipcode, 1)]
+    
+        #Aggregate transformation for zipCount
                 
-        print(zipCount)        
-        keys = {k[0] for k in zipCount}
-        agg_val= [(key, sum([n for (z,n) in zipCount if z == key])) for key in keys]
+        keys = {r[0] for r in zipCount}
+        aggregate_val= [(key, sum([v for (k,v) in zipCount if k == key])) for key in keys]
 
         final= []
-        for entry in agg_val:
-            final.append({'publicschoolsZipcode:':entry[0], 'schoolsCount':entry[1]})
-            print(final)
+        for entry in aggregate_val:
+            final.append({'schoolsZipcode:':entry[0], 'schoolsCount':entry[1]})
 
-        #repo['ashleyyu_bzwtong.aggpublicschools'].insert_many(final)
+        repo['ashleyyu_bzwtong.aggpublicschools'].insert_many(final)
         
         for entry in repo.ashleyyu_bzwtong.aggpublicschools.find():
              print(entry)
@@ -69,21 +67,24 @@ class aggpublicschools(dml.Algorithm):
         doc.add_namespace('dat', 'http://datamechanics.io/data/') # The data sets are in <user>#<collection> format.
         doc.add_namespace('ont', 'http://datamechanics.io/ontology#') # 'Extension', 'DataResource', 'DataSet', 'Retrieval', 'Query', or 'Computation'.
         doc.add_namespace('log', 'http://datamechanics.io/log/') # The event log.
+        doc.add_namespace('bdp', 'https://data.cityofboston.gov/resource/')
+        doc.add_namespace('bdp1', 'https://data.nlc.org/resource/')
+        doc.add_namespace('bdp2', 'https://data.boston.gov/export/622/208/')
 
 
-        this_script = doc.agent('alg:ashleyyu_bzwtong#aggpublicschools', {prov.model.PROV_TYPE:prov.model.PROV['SoftwareAgent'], 'ont:Extension':'py'})
+        this_script = doc.agent('alg:ashleyyu_bzwtong#schoolsAgg', {prov.model.PROV_TYPE:prov.model.PROV['SoftwareAgent'], 'ont:Extension':'py'})
         
-        resource_properties = doc.entity('dat:ashleyyu_bzwtong#publicschools', {'prov:label':' Public Schools Aggregate Zips', prov.model.PROV_TYPE:'ont:DataResource', 'ont:Extension':'json'})
-        get_aggpublicschools = doc.activity('log:uuid'+str(uuid.uuid4()), startTime, endTime)
+        resource_properties = doc.entity('dat:ashleyyu_bzwtong#schools', {'prov:label':' Public Schools Aggregate Zips', prov.model.PROV_TYPE:'ont:DataResource', 'ont:Extension':'json'})
+        get_schoolsAgg = doc.activity('log:uuid'+str(uuid.uuid4()), startTime, endTime)
         doc.wasAssociatedWith(get_aggpublicschools, this_script)
         doc.usage(get_aggpublicschools, resource_properties, startTime,None,
                   {prov.model.PROV_TYPE:'ont:Computation'})
 
 
-        aggpublicschools = doc.entity('dat:ashleyyu_bzwtong#aggpublicschools', {prov.model.PROV_LABEL:' Public Schools Aggregate Zips', prov.model.PROV_TYPE:'ont:DataSet'})
-        doc.wasAttributedTo(aggpublicschools, this_script)
-        doc.wasGeneratedBy(aggpublicschools, get_aggpublicschools, endTime)
-        doc.wasDerivedFrom(aggpublicschools, resource_properties, get_aggpublicschools, get_aggpublicschools, get_aggpublicschools)
+        schoolsAgg = doc.entity('dat:ashleyyu_bzwtong#schoolsAgg', {prov.model.PROV_LABEL:' Public Schools Aggregate Zips', prov.model.PROV_TYPE:'ont:DataSet'})
+        doc.wasAttributedTo(schoolsAgg, this_script)
+        doc.wasGeneratedBy(schoolsAgg, get_schoolsAgg, endTime)
+        doc.wasDerivedFrom(schoolsAgg, resource_properties, get_schoolsAgg, get_schoolsAgg, get_schoolsAgg)
 
 
 
@@ -91,7 +92,7 @@ class aggpublicschools(dml.Algorithm):
                   
         return doc
 
-aggpublicschools.execute()
-doc = aggpublicschools.provenance()
-print(doc.get_provn())
-print(json.dumps(json.loads(doc.serialize()), indent=4))
+#aggpublicschools.execute()
+#doc = aggpublicschools.provenance()
+#print(doc.get_provn())
+#print(json.dumps(json.loads(doc.serialize()), indent=4))

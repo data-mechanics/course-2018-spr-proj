38b52d1e73059d4ad060f48e1848f95a4f203d9e
Po-Yu Hsieh <pykenny@gmail.com>
2018-02-14 20:44:54 -0500

    Add retrieval algorithm and two transform algorithms

---
+++ liwang_pyhsieh/infras_rating.py
@@ -0,0 +1,190 @@
+import json
+import dml
+import uuid
+import prov.model
+from datetime import datetime
+import pandas
+from pyproj import Proj, transform
+from geopy.distance import vincenty
+
+
+def project(s, mapping):
+    result = []
+    for obj_s in s:
+        dicttmp = {}
+        for idx in mapping:
+            dicttmp[mapping[idx]] = obj_s[idx]
+        result.append(dicttmp)
+    return result
+
+
+def selection(s, f):
+    return [s for s in s if f(s)]
+
+
+def product(S, R, s_prefix, r_prefix, idx_s, idx_r):
+    result = []
+    for obj_s in S:
+        for obj_r in R:
+            dicttmp = {"_id": "_".join([s_prefix, str(obj_s[idx_s]), r_prefix, str(obj_r[idx_r])])}
+            for key_s in obj_s:
+                dicttmp[s_prefix + "_" + key_s] = obj_s[key_s]
+            for key_r in obj_r:
+                dicttmp[r_prefix + "_" + key_r] = obj_r[key_r]
+            result.append(dicttmp)
+    return result
+
+
+def join(S, R, s_prefix, r_prefix, s_idx, mcol_s, mcol_r, mcol_new):
+    result = []
+    for obj_s in S:
+        for obj_r in R:
+            if obj_s[mcol_s] == obj_r[mcol_r]:
+                dicttmp = {"_id": obj_s[s_idx]}
+                for key_s in obj_s:
+                    if key_s != mcol_s:
+                        dicttmp[s_prefix + "_" + key_s] = obj_s[key_s]
+                for key_r in obj_r:
+                    if key_r != mcol_r:
+                        dicttmp[r_prefix + "_" + key_r] = obj_r[key_r]
+                dicttmp[mcol_new] = obj_s[mcol_s]
+                result.append(dicttmp)
+    return result
+
+
+def group_aggcount(D, idx, g_col):
+    res = pandas.DataFrame.from_records(D, idx).groupby([g_col]).size()
+    return [{"_id": i, "count": r} for i, r in res.iteritems()]
+
+def group_aggsum(D, idx, g_col, s_col):
+    res = pandas.DataFrame.from_records(D, idx).groupby([g_col])[s_col].agg(["sum"])
+    idxlist = res.index.values
+    return [ {"_id": idxlist[i], "sum": row["sum"]} for i, row in enumerate(res.to_dict("records"))]
+
+def rename(D, colname_old, colname_new):
+    for item in D:
+        if colname_old in item:
+            item[colname_new] = item[colname_old]
+            del item[colname_old]
+    return
+
+
+def str2Datetime(date, time):
+    return datetime.strptime(" ".join([date, time]), "%d-%b-%Y %I:%M %p")
+
+
+def isNighttime(dtobj):
+    return dtobj.hour < 6 or dtobj.hour > 17
+
+
+def epsg2LonLat(x, y):
+    inproj = Proj(init='epsg:26986')  # EPSG for MA
+    outproj = Proj(init="epsg:4326")  # EPSG for world map
+    return transform(inproj, outproj, x, y)
+
+
+# Return distance between two points (Latitude, Longitude)
+# The distance is represented in kilometers
+def getVDist(lat1, long1, lat2, long2):
+    return vincenty((lat1, long1), (lat2, long2)).kilometers
+
+
+# Finding road safety rating by using numbers of
+# surrounding traffic signals and road lights
+class SafetyRating(dml.Algorithm):
+    startTime = datetime.now()
+
+    contributor = 'liwang_pyhsieh'
+    reads = ['liwang_pyhsieh.crash_2015', 'liwang_pyhsieh.street_lights',
+             'liwang_pyhsieh.traffic_signals']
+
+    writes = ['liwang_pyhsieh.safety_scores']
+
+    @staticmethod
+    def execute(trial=False):
+        # Set up the database connection.
+        client = dml.pymongo.MongoClient()
+        repo = client.repo
+        repo.authenticate('liwang_pyhsieh', 'liwang_pyhsieh')
+
+        dataset_crash = []
+        # Pull out all, select, then project part of the columns
+        for dataobj in repo['liwang_pyhsieh.crash_2015'].find():
+            str_date = dataobj["Crash Date"] + " " + dataobj["Crash Time"]
+            dateobj_date = datetime.strptime(str_date, "%d-%b-%Y %I:%M %p")
+            if isNighttime(dateobj_date):
+                latitude, longitude = epsg2LonLat(dataobj["X Coordinate"], dataobj["Y Coordinate"])
+                dataset_crash.append({
+                    "_id": dataobj["Crash Number"],
+                    "time": dateobj_date,
+                    "lat": latitude,
+                    "long": longitude
+                })
+
+
+        # Join and caculate distance of pairs
+        dataset_lights = [
+            {"_id": row["OBJECTID"], "Lat": row["Lat"], "Long": row["Long"]}
+            for row in repo['liwang_pyhsieh.street_lights'].find()
+        ]
+
+        '''
+        prod_crash_lights = product(dataset_crash, dataset_lights, "crash", "light", "_id", "_id")
+
+        prod_crash_lights = [
+            {"_id": row["_id"], "_id_crash": row["crash__id"], "_id_light": row["light__id"],
+             "dist": getVDist(row["crash_lat"], row["crash_long"], row["light_Lat"], row["light_Long"])}
+            for row in prod_crash_lights
+        ]
+
+        # Select ones within some range
+        near_lights = [
+            row for row in prod_crash_lights if row["dist"] < 0.25
+        ]
+
+        # Aggregate by sum
+        lightcounts = group_aggcount(near_lights, "_id", "_id_crash")
+        '''
+        # Do them again on signals
+        dataset_signals = [
+            {
+                "_id": row["properties"]["OBJECTID"],
+                "Lat": row["geometry"]["coordinates"][0],
+                "Long": row["geometry"]["coordinates"][1]
+            }
+            for row in repo['liwang_pyhsieh.traffic_signals'].find()
+        ]
+        prod_crash_signals = product(dataset_crash, dataset_signals, "crash", "signal", "_id", "_id")
+        prod_crash_signals = [
+            {"_id": row["_id"], "_id_crash": row["crash__id"], "_id_light": row["signal__id"],
+             "dist": getVDist(row["crash_lat"], row["crash_long"], row["signal_Lat"], row["signal_Long"])}
+            for row in prod_crash_signals
+        ]
+        near_signals = [
+            row for row in prod_crash_signals if row["dist"] < 0.25
+        ]
+
+        # Items with zero count will disappear, so zero-filling is required
+        # Encoding problem may happen when handling integers, so a conversion to string is made
+        signalcounts = group_aggcount(near_signals, "_id", "_id_crash")
+        signalcounts = signalcounts + [{"_id": row["_id"], "count": 0} for row in dataset_crash]
+        signalcounts = [{"_id": int(row["_id"]), "signal_count": int(row["sum"])} for row in group_aggsum(signalcounts, "_id", "_id", "count")]
+
+        print(signalcounts[:10])
+
+        '''
+        # Join them on _id
+        data_lightsignalcount = join(lightcounts, signalcounts, "light", "signal", "_id", "_id", "_id", "_id")
+        '''
+        # Store the result
+        repo.dropCollection("safety_scores")
+        repo.createCollection("safety_scores")
+        # repo['liwang_pyhsieh.crash_2015'].insert_many(data_lightsignalcount)
+        repo['liwang_pyhsieh.safety_scores'].insert_many(signalcounts)
+        repo.logout()
+
+    @staticmethod
+    def provenance(doc=prov.model.ProvDocument(), startTime=None, endTime=None):
+        pass
+
+SafetyRating.execute()
---
+++ liwang_pyhsieh/kmeans_accidents.py
@@ -0,0 +1,121 @@
+import json
+import dml
+import uuid
+import prov.model
+from datetime import datetime
+import pandas
+from pyproj import Proj, transform
+from geopy.distance import vincenty
+from numpy.random import uniform
+
+def str2Datetime(date, time):
+    return datetime.strptime(" ".join([date, time]), "%d-%b-%Y %I:%M %p")
+
+def epsg2LonLat(x, y):
+    inproj = Proj(init='epsg:26986')  # EPSG for MA
+    outproj = Proj(init="epsg:4326")  # EPSG for world map
+    return transform(inproj, outproj, x, y)
+
+def isNighttime(dtobj):
+    return dtobj.hour < 6 or dtobj.hour > 17
+
+# Return distance between two points (Latitude, Longitude)
+# The distance is represented in kilometers
+def dist(p1, p2):
+    return pow(p1[0] - p2[0], 2) + pow(p1[1] - p2[1], 2)
+
+def plus(args):
+    p = [0,0]
+    for _, (x,y) in args:
+        p[0] += x
+        p[1] += y
+    return tuple(p)
+
+def scale(p, c):
+    (x,y) = p
+    return (x/c, y/c)
+
+def product(R, S):
+    return [(t,u) for t in R for u in S]
+
+def aggregate(R, f):
+    keys = {r[0] for r in R}
+    return [(key, f([v for (k,v) in R if k == key])) for key in keys]
+
+# Finding road safety rating by using numbers of
+# surrounding traffic signals and road lights
+class KmeansForAccidentDist(dml.Algorithm):
+    contributor = 'liwang_pyhsieh'
+    reads = ['liwang_pyhsieh.crash_2015']
+    writes = ['liwang_pyhsieh.crash_2015_clustergroup']
+
+    @staticmethod
+    def execute(trial=False):
+        # Set up the database connection.
+        client = dml.pymongo.MongoClient()
+        repo = client.repo
+        repo.authenticate('liwang_pyhsieh', 'liwang_pyhsieh')
+
+        dataset_crash = []
+
+        # Pull out all, select, then project part of the columns
+        for dataobj in repo['liwang_pyhsieh.crash_2015'].find():
+            str_date = dataobj["Crash Date"] + " " + dataobj["Crash Time"]
+            dateobj_date = datetime.strptime(str_date, "%d-%b-%Y %I:%M %p")
+            if isNighttime(dateobj_date):
+                latitude, longitude = (dataobj["X Coordinate"], dataobj["Y Coordinate"])
+                dataset_crash.append((
+                    dataobj["Crash Number"],
+                    (latitude, longitude)
+                ))
+
+        # The example here uses the k-means algorithm in class
+        # For generating random initial medians
+        lat_col = [pos[0] for _, pos in dataset_crash]
+        lat_min, lat_max = min(lat_col), max(lat_col)
+        long_col = [pos[1] for _, pos in dataset_crash]
+        long_min, long_max = min(long_col), max(long_col)
+        num_cluster = 5
+        medians = [(i, p) for i, p in enumerate(list(zip(uniform(lat_min, lat_max, num_cluster), uniform(long_min, long_max, num_cluster))))]
+        points = dataset_crash
+        medians_old = []
+        MP = []
+        iter_ct, iter_limit = 0, 20
+        while medians_old != medians or iter_limit == iter_ct:
+            medians_old = medians
+
+            MPD = [(m, p, dist(m[1], p[1])) for (m, p) in product(medians, points)]
+            PDs = [(p, d) for (m, p, d) in MPD]
+            PD = aggregate(PDs, min)
+            MP = [(m, p) for ((m, p, d), (p2, d2)) in product(MPD, PD) if p[0] == p2[0] and d == d2]
+            MT = aggregate(MP, plus)
+
+            M1 = [(m, 1) for (m, _) in MP]
+            MC = aggregate(M1, sum)
+
+            medians = [(m[0], scale(t, c)) for ((m, t), (m2, c)) in product(MT, MC) if m == m2]
+            medians.sort()
+            iter_ct = iter_ct + 1
+
+        # Now, MP records the result
+        data_cluster = [
+            {
+                "_id": p[0],
+                "cluster_id": m[0],
+                "coordinate": {"Lat": epsg2LonLat(m[1][0], m[1][1])[0], "Long": epsg2LonLat(m[1][0], m[1][1])[1]}
+            }
+            for m, p in MP]
+        for i in range(50):
+            print(data_cluster[i])
+            
+        # Store the result
+        repo.dropCollection("crash_clustering")
+        repo.createCollection("crash_clustering")
+        # repo['liwang_pyhsieh.crash_2015'].insert_many(data_lightsignalcount)
+        repo['liwang_pyhsieh.crash_clustering'].insert_many(data_cluster)
+        repo.logout()
+    @staticmethod
+    def provenance(doc=prov.model.ProvDocument(), startTime=None, endTime=None):
+        pass
+
+KmeansForAccidentDist.execute()

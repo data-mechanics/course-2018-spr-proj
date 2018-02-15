dd4fffec0f01dff39d3837f6463349aad6cd4022
Jordan <jlove@bu.edu>
2018-02-14 23:48:46 -0500

    Project 1 Submission

---
+++ jlove/README.md
@@ -0,0 +1,5 @@
+With sea level rising along the Boston coast, coastal flooding is a very big problem in Boston. The question that I would like to answer is what kinds of people will be most affected by coastal flooding. More specifically, whether flooding will have a greater impact on people that are more or less wealthy. 
+
+To answer this question, I have gathered data on median household income by neighborhood, an estimation of how coastal flooding will impact Boston in 2070, imformation on the location of fire hydrants in Boston, the borders for the neighborhoods, and the elevation countours of Boston. 
+
+The flood data paired with the nieghborhood boundries will show me which neighborhoods will be most affected by coastal flooding, while the income data will tell me about the wealth of the people who live in these neighborhoods. I plan on using the firehydrant data as a way to very roughly quantize the effect of the flooding because fire hydrants a fairly evenly distributed among neighborhoods. The elevation data can give me an idea of the effects of the flooding beyond the 2070 flooding data.
---
+++ jlove/estimateNeighborhoodCoverage.py
@@ -0,0 +1,99 @@
+import dml
+import prov.model
+import datetime
+import uuid
+import shapely.geometry
+import numpy
+from tqdm import tqdm
+
+class estimateNeighborhoodCoverage(dml.Algorithm):
+    contributor = 'jlove'
+    reads = ['jlove.hydrantGroups', 'jlove.flood', 'jlove.hydrantCounts']
+    writes = ['jlove.percentCovered']
+    
+    @staticmethod
+    def execute(trial = False):
+        startTime = datetime.datetime.now()
+        client = dml.pymongo.MongoClient()
+        repo = client.repo
+        repo.authenticate('jlove', 'jlove')
+        flood = repo['jlove.flood'].find_one({})
+        hydrantCounts = repo['jlove.hydrantCounts'].find_on({})
+        hydrantGroups = repo['jlove.hydrantGroups'].find({})
+        
+        floodShape = shapely.geometry.shape(flood['features'][0]['geometry'])
+        covered = {}
+        for group in tqdm(hydrantGroups):
+            name = group['metadata']['neighborhood']
+            count = 0
+            for hydrant in tqdm(group['features']):
+                point = shapely.geometry.shape(hydrant['geometry'])
+                if floodShape.contains(point):
+                    count += 1
+            covered[name] = count
+        print(covered)
+        
+        for neighborhood in hydrantCounts.keys():
+            covered = covered[neighborhood]
+            try:
+                covered[neighborhood] = (covered/hydrantCounts[neighborhood]) * 100
+            except ZeroDivisionError:
+                covered[neighborhood] = 0
+        
+        repo.dropColection('percentCovered')
+        repo.createCollection('percentCovered')
+        
+        repo['jlove.percentCovered'].insert_one(covered)
+        
+        repo.logout()
+        
+        endTime = datetime.datetime.now()
+        return {"start":startTime, "end":endTime}
+        
+        
+    @staticmethod
+    def provenance(doc = prov.model.ProvDocument(), startTime = None, endTime = None):
+        client = dml.pymongo.MongoClient()
+        repo = client.repo
+        repo.authenticate('jlove', 'jlove')
+        doc.add_namespace('alg', 'http://datamechanics.io/algorithm/') # The scripts are in <folder>#<filename> format.
+        doc.add_namespace('dat', 'http://datamechanics.io/data/') # The data sets are in <user>#<collection> format.
+        doc.add_namespace('ont', 'http://datamechanics.io/ontology#') # 'Extension', 'DataResource', 'DataSet', 'Retrieval', 'Query', or 'Computation'.
+        doc.add_namespace('log', 'http://datamechanics.io/log/') # The event log.
+        doc.add_namespace('bdp', 'https://data.cityofboston.gov/dataset/')
+        
+        
+        this_script = doc.agent('alg:jlove#estimateNeighborhoodCoverage', {prov.model.PROV_TYPE:prov.model.PROV['SoftwareAgent'], 'ont:Extension':'py'})
+        
+        resource1 = doc.entity('dat:jlove#hydrantGroups', {'prov:label':'311, Service Requests', prov.model.PROV_TYPE:'ont:DataResource', 'ont:Extension':'geojson'})
+        estimate_coverage = doc.activity('log:uuid'+str(uuid.uuid4()), startTime, endTime)
+        doc.wasAssociatedWith(estimate_coverage, this_script)
+        doc.usage(estimate_coverage, resource1, startTime, None,
+                  {prov.model.PROV_TYPE:'ont:Retrieval'
+                  }
+                  )
+        resource2 = doc.entity('dat:jlove#flood', {'prov:label':'311, Service Requests', prov.model.PROV_TYPE:'ont:DataResource', 'ont:Extension':'geojson'})
+        doc.wasAssociatedWith(estimate_coverage, this_script)
+        doc.usage(estimate_coverage, resource2, startTime, None,
+                  {prov.model.PROV_TYPE:'ont:Retrieval'
+                  }
+                  )
+        resource3 = doc.entity('dat:jlove#hydrantCounts', {'prov:label':'311, Service Requests', prov.model.PROV_TYPE:'ont:DataResource', 'ont:Extension':'json'})
+        doc.wasAssociatedWith(estimate_coverage, this_script)
+        doc.usage(estimate_coverage, resource3, startTime, None,
+                  {prov.model.PROV_TYPE:'ont:Retrieval'
+                  }
+                  )
+
+        percent_covered = doc.entity('dat:jlove#percentCovered', {prov.model.PROV_LABEL:'Normalized Boston Median Household Income by Neighborhood', prov.model.PROV_TYPE:'ont:DataSet'})
+        doc.wasAttributedTo(percent_covered, this_script)
+        doc.wasGeneratedBy(percent_covered, estimate_coverage, endTime)
+        doc.wasDerivedFrom(percent_covered, resource1, estimate_coverage, estimate_coverage, estimate_coverage)
+        doc.wasDerivedFrom(percent_covered, resource2, estimate_coverage, estimate_coverage, estimate_coverage)
+        doc.wasDerivedFrom(percent_covered, resource3, estimate_coverage, estimate_coverage, estimate_coverage)
+       
+
+
+        repo.logout()
+                  
+        return doc

eddc0c347787e92a106ea44258bb27f9bba0db0a
Kai Bernardini <kbernardini@Kais-MBP.home>
2018-02-14 21:14:36 -0500

    311 is done

--- kaidb_vilin/three_one_one.py
+++ kaidb_vilin/three_one_one.py
@@ -15,6 +15,40 @@ class three_one_one(dml.Algorithm):
 
 
     @staticmethod
+    def clean_transform_data(df):
+        """Project data, and clean empty values"""
+        # Features used to determine the 
+        # Startic features of interest. 
+        # Locational features 
+        location_columns = ['precinct', 'LOCATION_ZIPCODE', 'Latitude', 'Longitude']
+        # complaint types 
+        descriptor = ['TYPE', 'REASON']
+        # date time objects 
+        date_cols = ['open_dt', 'target_dt','closed_dt']
+        # Note ontime doesn't mean the case is closed. 
+        # It means it is slated to finish before the estimate
+        punctual = ['OnTime_Status', 'CASE_STATUS']
+        # unique identifier 
+        identif = ['CASE_ENQUIRY_ID']
+
+        # Transfrom datatype of features to Date time objects
+        df[date_cols] = df[date_cols].apply(pd.to_datetime, errors='coerce')
+        # time to complete the task 
+        # will create NAAN values for open tasks 
+        completion_time = df['closed_dt'] - df['open_dt']
+        # Deviation from predicted allotment of time
+        estimate_deviation = df_c['closed_dt'] - df_c['target_dt']
+        
+        # Project out unecesarry meta-data
+        data = df[identif + date_cols + descriptor + location_columns + punctual]
+        data['completion_time'] = completion_time
+
+        # Each case is given an estimated required date of completion
+        # This computes the difference in actual execution time and estimated 
+        data['deviation_from_estimate'] = estimate_deviation
+        return data
+
+    @staticmethod
     def execute(trial = False, custom_url=None):
         '''Retrieve some data sets (not using the API here for the sake of simplicity).'''
         startTime = datetime.datetime.now()
@@ -26,34 +60,31 @@ class three_one_one(dml.Algorithm):
         repo.authenticate('kaidb_vilin', 'kaidb_vilin')
 
         # Updated daily
-        base = "https://data.boston.gov/"
-        api = "api/3/action/datastore_search?resource_id="
-        r_id = "2968e2c0-d479-49ba-a884-4ef523ada3c0"
-        
-        # Size per batch
-        l = 1000
-        lim = '&limit=' + str(l)
-        # initial URL 
-        url =  api + r_id+lim 
 
-        #  number of batches
-        pages = 10
+        url_311 = "https://data.boston.gov/dataset/8048697b-ad64-4bfc-b090-ee00169f2323/resource/2968e2c0-d479-49ba-a884-4ef523ada3c0/download/311.csv"
 
-        data = []
-        for i in range(pages): 
-            response =  urllib.request.urlopen(base + url ).read().decode("utf-8")
-            r = json.loads(response)
-            s = json.dumps(r, sort_keys=True, indent=2)
-            url = r['result']['_links']['next']
-            a = r['result']
-            print("Adding Batch ", i)
-            data.append(r['result']['records'][0])
+        # retrieve data using pandas 
+        data = pd.read_csv(url_311, encoding = "ISO-8859-1")
+        # puts data in useable format 
+        # projects out several columns
+        # Adds new columns to measure time-delta for completed taks. 
+        data = three_one_one.clean_transform_data(data)
 
+        # Obj transformation: 
+        #  df --> string (json formated) --> json 
+        r = json.loads(data.to_json( orient='records'))
+        # dump json. 
+        # Keys should already be sorted
+
+        # formated string. 
+        #s = json.dumps(r, sort_keys=True, indent=2)
+
+        ######
 
         repo.dropCollection("three_one_one")
         repo.createCollection("three_one_one")
 
-        repo['kaidb_vilin.three_one_one'].insert_many(data)
+        repo['kaidb_vilin.three_one_one'].insert_many(r)
 
         
         repo['kaidb_vilin.three_one_one'].metadata({'complete':True})
@@ -82,7 +113,7 @@ class three_one_one(dml.Algorithm):
         doc.add_namespace('ont', 'http://datamechanics.io/ontology#') # 'Extension', 'DataResource', 'DataSet', 'Retrieval', 'Query', or 'Computation'.
         doc.add_namespace('log', 'http://datamechanics.io/log/') # The event log.
         doc.add_namespace('dbd', 'https://data.boston.gov/dataset/')
-        doc.add_namespace('rc', '311')
+        doc.add_namespace('rc', '8048697b-ad64-4bfc-b090-ee00169f2323/resource/2968e2c0-d479-49ba-a884-4ef523ada3c0/download/311')
 
         this_script = doc.agent('alg:kaidb_vilin#three_one_one', {prov.model.PROV_TYPE:prov.model.PROV['SoftwareAgent'], 'ont:Extension':'py'})
 
@@ -93,7 +124,7 @@ class three_one_one(dml.Algorithm):
         # How to retrieve the .csv
         doc.usage(get_three_one_one, resource, startTime, None,
                   {prov.model.PROV_TYPE:'ont:Retrieval',
-                  'ont:Query':'api/3/action/datastore_search?resource_id=2968e2c0-d479-49ba-a884-4ef523ada3c0'
+                  'ont:Query':'8048697b-ad64-4bfc-b090-ee00169f2323/resource/2968e2c0-d479-49ba-a884-4ef523ada3c0/download/311.csv'
                   }
                   )
      

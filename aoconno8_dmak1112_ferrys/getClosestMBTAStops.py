dfbdecebb37af730192472227f0d24eff9924101
ferrys <ferrys@bu.edu>
2018-03-24 15:21:29 -0400

    Getting x closest mbta stops instead of all mbta stops within a distance

--- aoconno8_dmak1112_ferrys/getClosestMBTAStops.py
+++ aoconno8_dmak1112_ferrys/getClosestMBTAStops.py
@@ -5,15 +5,16 @@ import prov.model
 import datetime
 import uuid
 from tqdm import tqdm
+import rtree
+import shapely.geometry
 
-class getMBTADistances(dml.Algorithm):
+class getClosestMBTAStops(dml.Algorithm):
     '''
-        Returns the number of MBTA stops near a specific alcohol license
-        Easily extended to also return which MBTA stops are near and where they are
+        Returns the closest x mbta stops 
     '''
     contributor = 'aoconno8_dmak1112_ferrys'
     reads = ['aoconno8_dmak1112_ferrys.mbta', 'aoconno8_dmak1112_ferrys.alc_licenses']
-    writes = ['aoconno8_dmak1112_ferrys.mbtadistance']
+    writes = ['aoconno8_dmak1112_ferrys.closest_mbta_stops']
 
     @staticmethod
     def execute(trial = False):
@@ -25,55 +26,69 @@ class getMBTADistances(dml.Algorithm):
         repo.authenticate('aoconno8_dmak1112_ferrys', 'aoconno8_dmak1112_ferrys')
         api_key = dml.auth['services']['googlegeocoding']['key']
         
+        
         # mbta
+        num_mbta_stops = 3
         mbta = repo.aoconno8_dmak1112_ferrys.mbta.find()
-        projected_mbta = getMBTADistances.project(mbta, lambda t: (t['attributes']['latitude'], t['attributes']['longitude']))
+        projected_mbta = getClosestMBTAStops.project(mbta, lambda t: (t['attributes']['latitude'], t['attributes']['longitude']))
 
 
         # alc
         alc = repo.aoconno8_dmak1112_ferrys.alc_licenses.find()
-        projected_alc = getMBTADistances.project(alc, lambda t: (t['License Number'], t['Street Number'] + ' ' + t['Street Name'] + ' ' +  str(t['Suffix']) + ' ' + t['City']))
+        projected_alc = getClosestMBTAStops.project(alc, lambda t: (t['License Number'], t['Street Number'] + ' ' + t['Street Name'] + ' ' +  str(t['Suffix']) + ' ' + t['City']))
 
         if trial:
             projected_mbta = projected_mbta[:10]
             projected_alc = projected_alc[:10]
+            
+        index = rtree.index.Index()
+        for i in tqdm(range(len(projected_mbta))):
+            lat = projected_mbta[i][0]
+            lon = projected_mbta[i][1]
+            index.insert(i, shapely.geometry.Point(lon, lat).bounds)
 
         cache = {}
         mbta_dist = []
         for alc_entry in tqdm(projected_alc):
-            for mbta_entry in projected_mbta:
-                alc_address = alc_entry[1].replace(' ',  '+')
-                if alc_address in cache:
-                    alc_lat = cache[alc_address][0]
-                    alc_long = cache[alc_address][1]
-                else:
-                    alc_url = 'https://maps.googleapis.com/maps/api/geocode/json?address=' + alc_address + 'MA&key='+ api_key
-                    response = urllib.request.urlopen(alc_url).read().decode("utf-8")
-                    google_json = json.loads(response)
-                    try:
-                        alc_lat = google_json["results"][0]["geometry"]["location"]['lat']
-                        alc_long = google_json["results"][0]["geometry"]["location"]['lng']
-                        cache[alc_address] = (alc_lat,alc_long)
-                    except IndexError:
-                        print("Address not found")
-                        alc_lat = 0
-                        alc_long = 0
-                        cache[alc_address] = (alc_lat,alc_long)
-                
-                mbta_dist.append({
-                            "alc_license": alc_entry[0],
-                            "alc_coord":(alc_lat, alc_long),
-                            "mbta_coord":(mbta_entry[0], mbta_entry[1])
-                        })
+            
+            alc_address = alc_entry[1].replace(' ',  '+')
+            if alc_address in cache:
+                alc_lat = cache[alc_address][0]
+                alc_long = cache[alc_address][1]
+            else:
+                alc_url = 'https://maps.googleapis.com/maps/api/geocode/json?address=' + alc_address + 'MA&key='+ api_key
+                response = urllib.request.urlopen(alc_url).read().decode("utf-8")
+                google_json = json.loads(response)
+                try:
+                    alc_lat = google_json["results"][0]["geometry"]["location"]['lat']
+                    alc_long = google_json["results"][0]["geometry"]["location"]['lng']
+                    cache[alc_address] = (alc_lat,alc_long)
+                except IndexError:
+                    continue
+                    
+            # get x nearest mbta stops from the alc license
+            try:
+                for i in range(num_mbta_stops):
+                    nearest = index.nearest((alc_long,alc_lat,alc_long,alc_lat),  num_results=num_mbta_stops)
+            except TypeError:
+                pass
+            
+            mbta_coords = []
+            for point in nearest:
+                mbta_coords += [projected_mbta[point]]
+            
+            mbta_dist.append({
+                        "alc_license": alc_entry[0],
+                        "alc_coord":(alc_lat, alc_long),
+                        "mbta_coords":(mbta_coords)
+                    })
 
-        close_points = getMBTADistances.select(mbta_dist, lambda x: getMBTADistances.is_close((x['alc_coord'][0], x['alc_coord'][1]), (x['mbta_coord'][0], x['mbta_coord'][1])))
-        num_mbta_near = getMBTADistances.aggregate_mbta(close_points, lambda x: sum([1 for x in x]))
-
-        repo.dropCollection('mbtadistance')
-        repo.createCollection('mbtadistance')
-        repo['aoconno8_dmak1112_ferrys.mbtadistance'].insert_many(num_mbta_near)
-        repo['aoconno8_dmak1112_ferrys.mbtadistance'].metadata({'complete':True})
-        print(repo['aoconno8_dmak1112_ferrys.mbtadistance'].metadata())
+        print(mbta_dist)
+        repo.dropCollection("closest_mbta_stops")
+        repo.createCollection("closest_mbta_stops")
+        repo['aoconno8_dmak1112_ferrys.closest_mbta_stops'].insert_many(mbta_dist)
+        repo['aoconno8_dmak1112_ferrys.closest_mbta_stops'].metadata({'complete':True})
+        print(repo['aoconno8_dmak1112_ferrys.closest_mbta_stops'].metadata())
         
         repo.logout()
         endTime = datetime.datetime.now()
@@ -124,29 +139,13 @@ class getMBTADistances(dml.Algorithm):
         repo.logout()
         return doc
 
-    def union(R, S):
-        return R + S
-    def intersect(R, S):
-        return [t for t in R if t in S]
-    def product(R, S):
-        return [(t,u) for t in R for u in S]
-    def select(R, s):
-        return [t for t in R if s(t)]
-    def aggregate(R, f):
-        keys = {r[0] for r in R}
-        return [(key, f([v for (k,v) in R if k == key])) for key in keys]
-    def aggregate_mbta(R, f):
-        keys = {r['alc_license'] for r in R}
-        return [{key: f([v for v in R if v['alc_license'] == key])} for key in keys]
+
     def project(R, p):
         return [p(t) for t in R]
-    def is_close(mbta_stop, alc_license):
-        # around 1.5 miles apart
-        return abs(mbta_stop[0] - alc_license[0]) < .015 and abs(mbta_stop[1] - alc_license[1]) < .015
 
 
-#getMBTADistances.execute(True)
-#doc = getMBTADistances.provenance()
+getClosestMBTAStops.execute()
+#doc = getClosestMBTAStops.provenance()
 #print(doc.get_provn())
 #print(json.dumps(json.loads(doc.serialize()), indent=4))
 

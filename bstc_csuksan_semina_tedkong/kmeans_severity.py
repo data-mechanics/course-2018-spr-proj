63ffe7112a6bdbe86cbf960d09931d06940282ef
ASemina <34461262+ASemina@users.noreply.github.com>
2018-03-28 21:38:43 -0400

    Update dataset and create kmeans algorithm

---
+++ bstc_csuksan_semina_tedkong/kmeans_severity.py
@@ -0,0 +1,135 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 28 18:08:31 2018
+
+@author: Alexander
+"""
+
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb 11 16:32:25 2018
+
+@author: Alexander
+"""
+
+
+import urllib.request
+import json
+import dml
+import prov.model
+import datetime
+import uuid
+from time import sleep
+import sample
+import pandas as pd
+import sklearn.cluster as sk
+import matplotlib.pyplot as pyplot
+import numpy as np
+import scipy.io
+import scipy.misc
+
+class getBostonYelpRestaurantData(dml.Algorithm):
+    
+    contributor = "bstc_semina"
+    reads = []
+    writes = ['bstc_semina.getBostonYelpRestaurantData']
+    
+    
+    
+    
+    @staticmethod
+    def execute(trial = False):
+        startTime = datetime.datetime.now()
+#        
+#        client = dml.pymongo.MongoClient()
+#        repo = client.repo
+#        repo.authenticate('bstc_semina', 'bstc_semina')
+        
+        #collection = repo.bstc_semina.RestaurantRatingAndHealthViolations
+        #cursor = collection.find({})
+        
+        
+        
+        file = pd.read_json("merged_datasets/RestaurantRatingsAndHealthViolations_Boston.json", lines=True)
+#        severity = file['ave_violation_severity']
+#        rating = file['rating']
+#        longitude = file['longitude']
+#        latitude = file['latitude']
+        data = file[['ave_violation_severity', 'rating','latitude','longitude']].copy()
+        #arr = np.array([severity, rating])
+        #print(arr)
+        k = 10
+        arr = data[data.longitude > -75]
+        kmeans = sk.KMeans(n_clusters = k, random_state = 0).fit(arr[['ave_violation_severity','rating']])
+        #print(kmeans.cluster_centers_)
+        
+        centroids = kmeans.cluster_centers_
+        labels = kmeans.labels_
+        arr = np.array(arr[['latitude','longitude']])
+        print(arr)
+        for i in range(k):
+            # select only data observations with cluster label == i
+            
+            ds = arr[np.where(labels==i)]
+            # plot the data observations
+            
+            pyplot.plot(ds[:,0],ds[:,1],'o')
+            # plot the centroids
+#            lines = pyplot.plot(centroids[i,0],centroids[i,1],'kx')
+            # make the centroid x's bigger
+#            pyplot.setp(lines,ms=15.0)
+#            pyplot.setp(lines,mew=2.0)
+        pyplot.show()
+        
+        
+        #print(type(file))
+        
+#        repo.logout()
+        
+        endTime = datetime.datetime.now()
+        
+        return ({'start':startTime, 'end':endTime})
+    
+    
+    
+    def provenance(doc = prov.model.ProvDocument(), startTime = None, endTime = None):
+        '''
+            Create the provenance document describing everything happening
+            in this script. Each run of the script will generate a new
+            document describing that invocation event.
+            '''
+
+        # Set up the database connection.
+        client = dml.pymongo.MongoClient()
+        repo = client.repo
+        repo.authenticate('bstc_semina', 'bstc_semina')
+        doc.add_namespace('alg', 'http://datamechanics.io/algorithm/') # The scripts are in <folder>#<filename> format.
+        doc.add_namespace('dat', 'http://datamechanics.io/data/') # The data sets are in <user>#<collection> format.
+        doc.add_namespace('ont', 'http://datamechanics.io/ontology#') # 'Extension', 'DataResource', 'DataSet', 'Retrieval', 'Query', or 'Computation'.
+        doc.add_namespace('log', 'http://datamechanics.io/log/') # The event log.
+        doc.add_namespace('bdp', 'https://www.yelp.com/developers/')
+
+        this_script = doc.agent('alg:bstc_semina#getBostonYelpRestaurantData', {prov.model.PROV_TYPE:prov.model.PROV['SoftwareAgent'], 'ont:Extension':'py'})
+        resource = doc.entity('bdp:wc8w-nujj', {'prov:label':'Reviews', prov.model.PROV_TYPE:'ont:DataResource', 'ont:Extension':'json'})
+        get_rate = doc.activity('log:uuid'+str(uuid.uuid4()), startTime, endTime)
+        doc.wasAssociatedWith(get_rate, this_script)
+        doc.usage(get_rate, resource, startTime, None,
+                  {prov.model.PROV_TYPE:'ont:Retrieval',
+                  'ont:Query':'?type=Yelp+Reviews&$select=_id,businesses,total,region'
+                  }
+                  )
+
+        rate = doc.entity('dat:bstc_semina#getBostonYelpRestaurantData', {prov.model.PROV_LABEL:'Yelp Ratings', prov.model.PROV_TYPE:'ont:DataSet'})
+        doc.wasAttributedTo(rate, this_script)
+        doc.wasGeneratedBy(rate, get_rate, endTime)
+        doc.wasDerivedFrom(rate, resource, get_rate, get_rate, get_rate)
+
+
+        repo.logout()
+                  
+        return doc
+    
+getBostonYelpRestaurantData.execute()
+doc = getBostonYelpRestaurantData.provenance()
+#print(doc.get_provn())
+#print(json.dumps(json.loads(doc.serialize()), indent=4))

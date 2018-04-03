d407d2ff87e90ef9b2cbfce4d9f986b70d7ac690
ASemina <34461262+ASemina@users.noreply.github.com>
2018-04-02 20:35:23 -0400

    Added new files for project 2

---
+++ bstc_csuksan_semina_tedkong/BostonRestaurants_FullyConnectedMap.py
@@ -0,0 +1,173 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 29 20:12:03 2018
+
+@author: Alexander
+"""
+import dml
+import prov.model
+import datetime
+import uuid
+import pandas as pd
+import numpy as np
+import json
+
+class getBostonYelpRestaurantData(dml.Algorithm):
+    
+    contributor = "bstc_semina"
+    reads = []
+    writes = ['bstc_semina.getBostonYelpRestaurantData']
+    
+    
+    
+    
+    @staticmethod
+    def execute(trial = False):
+        startTime = datetime.datetime.now()
+        
+         """
+        
+        First read in the merged Rating and Health Violation data and form a 
+        box around the city of Boston to cut out blatantly wrong data
+        
+        """
+        
+        
+        file = pd.read_json("merged_datasets/RestaurantRatingsAndHealthViolations_Boston.json", lines=True)
+        
+        
+        
+        arr = file[['name', 'ave_violation_severity', 'rating','latitude','longitude']].copy()
+        #print(arr)
+        
+        top = [42.400549, -71.004839]
+        bot = [42.226935, -71.129931]
+        right = [42.326260, -70.921191]
+        left = [42.282590, -71.194209]
+
+        arr = arr[(arr.latitude <= top[0]) & (arr.latitude >= bot[0]) & (arr.longitude <= right[1]) & (arr.longitude >= left[1])]
+        #print(arr)
+        arr = arr.drop_duplicates()
+        arr = arr.sort_values(by='name')
+        data = np.array(arr[['name', 'latitude','longitude', 'ave_violation_severity']])
+        
+        """
+        
+        Create Dataframe that is a n^2 array of all connections
+        
+        """
+
+        distance = pd.DataFrame()
+
+        for i in data:
+            val = {'name':i[0]}
+            count =0
+            for j in data:
+                count +=1
+                if (i[0] == j[0]):
+                    val.update({i[0] + ' | ' + str(j[3] + j[1] + j[2]) :10000000})
+                else:
+                    x1 = i[2] * 100
+                    x2 = j[2] * 100
+                    y1 = i[1] * 100
+                    y2 = j[1] * 100
+                    d = np.sqrt(np.power(x1-x2, 2) + np.power(y1-y2, 2))
+                    val.update({(j[0]+ ' | ' +str(j[3] + j[1] + j[2])) : d})
+            di = pd.DataFrame([val])
+            distance = pd.concat([distance, di])
+            
+        js = distance.to_json(orient='records')[:].replace('},{', '}\n{')
+        js = js.replace('[','')
+        js = js.replace(']','')
+                
+        with open("BostonRestaurants_Map.json", 'w') as jf:
+            jf.write(js)
+
+
+        #Does the same as the before for loop, but takes 16 hours to run. 
+        #DO NOT RUN THIS 
+        """
+        distance = pd.DataFrame()
+        
+        for ind,row in data.iterrows():
+            val = pd.DataFrame([{'name' : row['name']}])
+            #print(val)
+            for ind2, row2 in data.iterrows():
+                if (row['name'] == row2['name']):
+                    dis = pd.DataFrame([{row['name'] + str(row2['ave_violation_severity'] + row2['latitude'] + row2['longitude']): 1000000}])
+                    val = val.join(dis)
+                else:
+                    y1 = row['latitude'] *100
+                    y2 = row2['latitude'] *100
+                    x1 = row['longitude'] *100
+                    x2 = row2['longitude'] *100
+                    d = np.sqrt(np.power(x1-x2, 2) + np.power(y1-y2, 2))
+                    dis = pd.DataFrame([{row2['name'] + str(row2['ave_violation_severity'] + row2['latitude'] + row2['longitude']): d}])
+                    val = val.join(dis)
+            distance = pd.concat([distance,val])
+            if (row['name'] == "Cappy's Pizza"):
+                print(distance)
+                break
+            
+
+        js = distance.to_json(orient='records')[:].replace('},{', '}\n{')
+        js = js.replace('[','')
+        js = js.replace(']','')
+        
+        
+        with open("BostonRestaurants_Map_TEST.json", 'w') as jf:
+            jf.write(js)
+        """
+        
+        #print(arr)
+        
+        
+#        repo.logout()
+        
+        endTime = datetime.datetime.now()
+        
+        return ({'start':startTime, 'end':endTime})
+    
+    
+    
+    def provenance(doc = prov.model.ProvDocument(), startTime = None, endTime = None):
+        '''
+            Create the provenance document describing everything happening
+            in this script. Each run of the script will generate a new
+            document describing that invocation event.
+            '''
+
+        # Set up the database connection.
+        client = dml.pymongo.MongoClient()
+        repo = client.repo
+        repo.authenticate('bstc_semina', 'bstc_semina')
+        doc.add_namespace('alg', 'http://datamechanics.io/algorithm/') # The scripts are in <folder>#<filename> format.
+        doc.add_namespace('dat', 'http://datamechanics.io/data/') # The data sets are in <user>#<collection> format.
+        doc.add_namespace('ont', 'http://datamechanics.io/ontology#') # 'Extension', 'DataResource', 'DataSet', 'Retrieval', 'Query', or 'Computation'.
+        doc.add_namespace('log', 'http://datamechanics.io/log/') # The event log.
+        doc.add_namespace('bdp', 'https://www.yelp.com/developers/')
+
+        this_script = doc.agent('alg:bstc_semina#getBostonYelpRestaurantData', {prov.model.PROV_TYPE:prov.model.PROV['SoftwareAgent'], 'ont:Extension':'py'})
+        resource = doc.entity('bdp:wc8w-nujj', {'prov:label':'Reviews', prov.model.PROV_TYPE:'ont:DataResource', 'ont:Extension':'json'})
+        get_rate = doc.activity('log:uuid'+str(uuid.uuid4()), startTime, endTime)
+        doc.wasAssociatedWith(get_rate, this_script)
+        doc.usage(get_rate, resource, startTime, None,
+                  {prov.model.PROV_TYPE:'ont:Retrieval',
+                  'ont:Query':'?type=Yelp+Reviews&$select=_id,businesses,total,region'
+                  }
+                  )
+
+        rate = doc.entity('dat:bstc_semina#getBostonYelpRestaurantData', {prov.model.PROV_LABEL:'Yelp Ratings', prov.model.PROV_TYPE:'ont:DataSet'})
+        doc.wasAttributedTo(rate, this_script)
+        doc.wasGeneratedBy(rate, get_rate, endTime)
+        doc.wasDerivedFrom(rate, resource, get_rate, get_rate, get_rate)
+
+
+        repo.logout()
+                  
+        return doc
+    
+getBostonYelpRestaurantData.execute()
+doc = getBostonYelpRestaurantData.provenance()
+#print(doc.get_provn())
+#print(json.dumps(json.loads(doc.serialize()), indent=4))
--- bstc_csuksan_semina_tedkong/kmeans_severity.py
+++ bstc_csuksan_semina_tedkong/kmeans_severity.py
@@ -27,6 +27,10 @@ import matplotlib.pyplot as pyplot
 import numpy as np
 import scipy.io
 import scipy.misc
+from bokeh.io import output_file, show
+from bokeh.models import (
+  GMapPlot, GMapOptions, ColumnDataSource, Circle, Range1d, PanTool, WheelZoomTool, BoxSelectTool
+)
 
 class getBostonYelpRestaurantData(dml.Algorithm):
     
@@ -47,7 +51,7 @@ class getBostonYelpRestaurantData(dml.Algorithm):
         
         #collection = repo.bstc_semina.RestaurantRatingAndHealthViolations
         #cursor = collection.find({})
-        
+        google_api_key = "AIzaSyBOG07I1r2Tn6Xye_eFgpZTbstjsj0kNfI"
         
         
         file = pd.read_json("merged_datasets/RestaurantRatingsAndHealthViolations_Boston.json", lines=True)
@@ -55,20 +59,65 @@ class getBostonYelpRestaurantData(dml.Algorithm):
 #        rating = file['rating']
 #        longitude = file['longitude']
 #        latitude = file['latitude']
-        data = file[['ave_violation_severity', 'rating','latitude','longitude']].copy()
+        arr = file[['ave_violation_severity', 'rating','latitude','longitude']].copy()
         #arr = np.array([severity, rating])
         #print(arr)
         k = 10
-        arr = data[data.longitude > -75]
+        #arr = data[data.longitude > -75]
         kmeans = sk.KMeans(n_clusters = k, random_state = 0).fit(arr[['ave_violation_severity','rating']])
         #print(kmeans.cluster_centers_)
         
         centroids = kmeans.cluster_centers_
         labels = kmeans.labels_
-        arr = np.array(arr[['latitude','longitude']])
-        print(arr)
+        
+        data = np.array(arr[['ave_violation_severity','rating']])
+        #arr = np.array(arr[['latitude','longitude']])
+        #print(arr)
+        #print(arr)
+        
+        
+        map_options = GMapOptions(lat=42.35, lng=-71.05, map_type="roadmap", zoom=11)
+        
+        plot = GMapPlot(x_range=Range1d(), y_range=Range1d(), map_options=map_options)
+        plot.title.text = "Boston"
+        
+        plot.api_key = google_api_key
+        
+        source = ColumnDataSource(
+            data=dict(
+                lat=np.array(arr[["latitude"]]),
+                lon=np.array(arr[["longitude"]]),
+            )
+        )
+        
+        circle = Circle(x="lon", y="lat", size=15, fill_color="blue", fill_alpha=0.8, line_color=None)
+        plot.add_glyph(source, circle)
+        
+        plot.add_tools(PanTool(), WheelZoomTool(), BoxSelectTool())
+        output_file("gmap_plot.html")
+        show(plot)
+        
+        
+        
         for i in range(k):
             # select only data observations with cluster label == i
+            pyplot.figure(1)
+            
+            ds = data[np.where(labels==i)]
+            # plot the data observations
+            
+            #pyplot.plot(ds[:,0],ds[:,1],'o')
+            # plot the centroids
+            #lines = pyplot.plot(centroids[i,0],centroids[i,1],'kx')
+            # make the centroid x's bigger
+            #pyplot.setp(lines,ms=15.0)
+            #pyplot.setp(lines,mew=2.0)
+        pyplot.show()
+        
+        """
+        for i in range(k):
+            # select only data observations with cluster label == i
+            pyplot.figure(2)
             
             ds = arr[np.where(labels==i)]
             # plot the data observations
@@ -80,7 +129,7 @@ class getBostonYelpRestaurantData(dml.Algorithm):
 #            pyplot.setp(lines,ms=15.0)
 #            pyplot.setp(lines,mew=2.0)
         pyplot.show()
-        
+        """
         
         #print(type(file))
         

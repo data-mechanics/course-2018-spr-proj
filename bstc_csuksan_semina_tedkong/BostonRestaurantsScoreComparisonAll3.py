6d739d3a495e7570d8987a81e7c373dd64efa11f
Tachapan Kongboonma (Ted) <tedkong@bu.edu>
2018-04-04 17:08:37 -0400

    add correlation JSON

--- bstc_csuksan_semina_tedkong/BostonRestaurantsScoreComparisonAll3.py
+++ bstc_csuksan_semina_tedkong/BostonRestaurantsScoreComparisonAll3.py
@@ -16,9 +16,9 @@ import matplotlib.pyplot as plt
 
 class BostonRestaurantsScoreComparison(dml.Algorithm):
     
-    contributor = "bstc_semina"
+    contributor = "bstc_csuksan_semina_tedkong"
     reads = []
-    writes = ['bstc_semina.BostonRestaurantsScoreComparison']
+    writes = ['bstc_csuksan_semina_tedkong.BostonRestaurantsScoreComparison']
     
     
     
@@ -111,11 +111,29 @@ class BostonRestaurantsScoreComparison(dml.Algorithm):
 
         #print(top_connections)  
 
-        #need score, severity                  
+        #Putting the scores comparison into dataframe                  
         df1 = pd.DataFrame({'My_Score':my_score_arr_combined, 'Their_Score':their_score_arr_combined})
         df2 = pd.DataFrame({'My_Score':my_score_arr_rating, 'Their_Score':their_score_arr_rating})
         df3 = pd.DataFrame({'My_Score':my_score_arr_violation, 'Their_Score':their_score_arr_violation})
-        #print(df)
+        
+
+        #printing and calculating the correlation
+        corr1 = df1['My_Score'].corr(df1['Their_Score'])
+        corr2 = df2['My_Score'].corr(df2['Their_Score'])
+        corr3 = df3['My_Score'].corr(df3['Their_Score'])
+        print(corr1)
+        print(corr2)
+        print(corr3)
+
+        correlation = {"Correlation": [{"Score": "Rating + Violation Severity", "Correlation": corr1}, 
+        {"Score": "Rating", "Correlation": corr2}, 
+        {"Score": "Violation Severity", "Correlation": corr3} ] }
+
+        with open('correlation_score.json', 'w') as fp:
+            json.dump(correlation, fp)
+
+
+        #print(correlation)
 
         df1.to_csv("CombinedScoreComparison.csv", encoding = 'utf-8')
         df2.to_csv("RatingScoreComparison.csv", encoding = 'utf-8')
@@ -157,14 +175,14 @@ class BostonRestaurantsScoreComparison(dml.Algorithm):
         # Set up the database connection.
         client = dml.pymongo.MongoClient()
         repo = client.repo
-        repo.authenticate('bstc_semina', 'bstc_semina')
+        repo.authenticate('bstc_csuksan_semina_tedkong', 'bstc_csuksan_semina_tedkong')
         doc.add_namespace('alg', 'http://datamechanics.io/algorithm/') # The scripts are in <folder>#<filename> format.
         doc.add_namespace('dat', 'http://datamechanics.io/data/') # The data sets are in <user>#<collection> format.
         doc.add_namespace('ont', 'http://datamechanics.io/ontology#') # 'Extension', 'DataResource', 'DataSet', 'Retrieval', 'Query', or 'Computation'.
         doc.add_namespace('log', 'http://datamechanics.io/log/') # The event log.
         doc.add_namespace('bdp', 'https://www.yelp.com/developers/')
 
-        this_script = doc.agent('alg:bstc_semina#BostonRestaurantsScoreComparison', {prov.model.PROV_TYPE:prov.model.PROV['SoftwareAgent'], 'ont:Extension':'py'})
+        this_script = doc.agent('alg:bstc_csuksan_semina_tedkong#BostonRestaurantsScoreComparison', {prov.model.PROV_TYPE:prov.model.PROV['SoftwareAgent'], 'ont:Extension':'py'})
         resource = doc.entity('bdp:wc8w-nujj', {'prov:label':'Reviews', prov.model.PROV_TYPE:'ont:DataResource', 'ont:Extension':'json'})
         get_rate = doc.activity('log:uuid'+str(uuid.uuid4()), startTime, endTime)
         doc.wasAssociatedWith(get_rate, this_script)
@@ -174,7 +192,7 @@ class BostonRestaurantsScoreComparison(dml.Algorithm):
                   }
                   )
 
-        rate = doc.entity('dat:bstc_semina#BostonRestaurantsScoreComparison', {prov.model.PROV_LABEL:'Yelp Ratings', prov.model.PROV_TYPE:'ont:DataSet'})
+        rate = doc.entity('dat:bstc_csuksan_semina_tedkong#BostonRestaurantsScoreComparison', {prov.model.PROV_LABEL:'Yelp Ratings', prov.model.PROV_TYPE:'ont:DataSet'})
         doc.wasAttributedTo(rate, this_script)
         doc.wasGeneratedBy(rate, get_rate, endTime)
         doc.wasDerivedFrom(rate, resource, get_rate, get_rate, get_rate)

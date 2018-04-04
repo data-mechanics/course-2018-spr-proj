81e359e79128ed2700863cca11e35c932efbe6de
ASemina <34461262+ASemina@users.noreply.github.com>
2018-04-04 18:30:34 -0400

    Added write to mongo collection

--- bstc_csuksan_semina_tedkong/BostonRestaurantsStatsAnalysis.py
+++ bstc_csuksan_semina_tedkong/BostonRestaurantsStatsAnalysis.py
@@ -35,10 +35,10 @@ class BostonRestaurantStatsAnalysis(dml.Algorithm):
     @staticmethod
     def execute(trial = False):
         startTime = datetime.datetime.now()
-#
-#        client = dml.pymongo.MongoClient()
-#        repo = client.repo
-#        repo.authenticate('bstc_csuksan_semina_tedkong', 'bstc_csuksan_semina_tedkong')
+
+        client = dml.pymongo.MongoClient()
+        repo = client.repo
+        repo.authenticate('bstc_csuksan_semina_tedkong', 'bstc_csuksan_semina_tedkong')
 
         #collection = repo.bstc_csuksan_semina_tedkong.RestaurantRatingAndHealthViolations
         #cursor = collection.find({})
@@ -49,7 +49,7 @@ class BostonRestaurantStatsAnalysis(dml.Algorithm):
         and because it didnt do anything for our understanding other than look nice.
         
         """
-        google_api_key = dml.auth['services']['google']['key']
+        #google_api_key = dml.auth['services']['google']['key']
 
 
         """
@@ -101,15 +101,13 @@ class BostonRestaurantStatsAnalysis(dml.Algorithm):
 
         #convert np to list
         centroids1_to_list = centroids.tolist()
-        centroids2_to_list = centroids.tolist()
+        centroids2_to_list = centroids2.tolist()
 
-        #create dict of centroids to be written as json
-        centroids_dict = {'KmeansCentroids': [{'SeverityAndRatingCentroids': centroids1_to_list,
-        'SeverityRatingLongLatCentroids': centroids2_to_list}]}
+
 
         #write as json
-        with open('kmeans_centroids.json', 'w') as outfile:
-            json.dump(centroids_dict, outfile)
+#        with open('kmeans_centroids.json', 'w') as outfile:
+#            json.dump(centroids_dict, outfile)
 
         """
 
@@ -150,9 +148,28 @@ class BostonRestaurantStatsAnalysis(dml.Algorithm):
         #save spearman's correlation to json
         spearman_dict = {"Correlation": [{"Spearman1": spear1_result[0], "P-Value1":spear1_result[1]}, 
         {"Spearman2": spear2_result[0], "P-Value2":spear2_result[1]}] }
+    
+        #create dict of centroids to be written as json
+        centroids_dict = {'KmeansCentroids': [{'SeverityAndRatingCentroids': centroids1_to_list,
+        'SeverityRatingLongLatCentroids': centroids2_to_list}]}
+    
+        new_collection_name = 'SpearmanRatingVsSeverity'
+    
+        repo.dropCollection('bstc_csuksan_semina_tedkong.'+new_collection_name)
+        repo.createCollection('bstc_csuksan_semina_tedkong.'+new_collection_name)
+        records = json.loads(pd.DataFrame(spearman_dict).to_json(orient='records'))
+        repo['bstc_csuksan_semina_tedkong.'+new_collection_name].insert_many(records)
+        
+        new_collection_name = 'CentroidsKmeans'
+    
+        repo.dropCollection('bstc_csuksan_semina_tedkong.'+new_collection_name)
+        repo.createCollection('bstc_csuksan_semina_tedkong.'+new_collection_name)
+        records = json.loads(pd.DataFrame(centroids_dict).to_json(orient='records'))
+        repo['bstc_csuksan_semina_tedkong.'+new_collection_name].insert_many(records)
+        
 
-        with open('spearman_correlation_score.json', 'w') as fp:
-            json.dump(spearman_dict, fp)
+#        with open('spearman_correlation_score.json', 'w') as fp:
+#            json.dump(spearman_dict, fp)
   
         """
 

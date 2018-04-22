99d056a241e218ff17a25864718aa9fc076f0914
alex-oconnor <aoconno8@bu.edu>
2018-04-22 14:41:28 -0400

    viz2

--- aoconno8_dmak1112_ferrys/visualization/DataToCsv.py
+++ aoconno8_dmak1112_ferrys/visualization/DataToCsv.py
@@ -9,33 +9,35 @@ response = urllib.request.urlopen(url).read().decode("utf-8")
 d = json.loads(response)
 
 
-optlist = []
 otherlist = []
 for i in range(len(d)):
     score = d[i]['optimal_route']['score']
     dist = d[i]['optimal_route']['route_dist']
     if d[i]['optimal_route']['route_dist'] > 8000:
         continue
+    if d[i]['optimal_route']['type'] == 'shortest':
+        symbol = 'SHORT'
+    else:
+        symbol = 'SAFE'
     if not math.isnan(d[i]['optimal_route']['score']):
-        optlist.append(("OPT", score, dist))
+        otherlist.append((symbol, score, dist))
     for i in d[i]['other_routes']:
+        if i['type'] == 'safest':
+            symbol = 'SAFE'
+        else:
+            symbol = 'SHORT'
         if i['route_dist'] > 8000:
-            continue
+                continue
         if not math.isnan(i['score']):
-            otherlist.append(("OTHER", i['score'], i['route_dist']))
+            otherlist.append((symbol, i['score'], i['route_dist']))
 otherlist = sorted(otherlist, key=lambda x: x[1])
-optlist = sorted(optlist, key=lambda x: x[1])
 
 
 
 
 
 full_list = [] 
-for i in range(len(optlist)):
-    symbol = optlist[i][0]
-    score = optlist[i][1]
-    dist = optlist[i][2]
-    full_list.append([symbol, score, dist])
+
 
 for i in range(len(otherlist)):
     symbol = otherlist[i][0]
@@ -45,6 +47,6 @@ for i in range(len(otherlist)):
 
 
 df = pd.DataFrame(full_list, columns=['symbol','date','price'])
-df.to_csv("data.csv",index=False)
+df.to_csv("output/data.csv",index=False)
 print("Done")
 

8e3c415069d09faa89d0a81e2ee6a0a38135a17d
dwang1995 <wangdayuan1995@gmail.com>
2018-04-25 19:08:49 -0400

    add the comparing page

--- debhe_shizhan0_wangdayu_xt/project3/app.py
+++ debhe_shizhan0_wangdayu_xt/project3/app.py
@@ -63,6 +63,52 @@ def schoolNewBikeHub():
 		return render_template("schoolNewBikeHub.html", schoolName = schoolName, subwayStationName = subwayName, school_x=schoolCorX, school_y=schoolCorY, subway_x=subwayCorX, subway_y=subwayCorY)
 	return render_template("schoolNewBikeHub.html")
 
+@app.route('/OnClickHubway/<schooName>', methods = ['GET'])
+def onClickHubway(schoolName):
+	client = dml.pymongo.MongoClient()
+	repo = client.repo
+	repo.authenticate('debhe_shizhan0_wangdayu_xt', 'debhe_shizhan0_wangdayu_xt')
+	schoolHubwayList = []
+	schoolHubwayList = repo['debhe_shizhan0_wangdayu_xt.schoolHubwayDistance'].find()
+	for row in schoolHubwayList:
+		if row['schoolName'] == schoolNameInput:
+			hubwayStationName = row['hubwayStation']
+			schoolCorX = row['School_Cor_x']
+			schoolCorY = row['School_Cor_y']
+			hubwayCorX = row['Hubway_Cor_x']
+			hubwayCorY = row['Hubway_Cor_y']
+	repo.logout()
+	return render_template("OnClickHubway.html", schoolName = schoolName, hubwayStationName = hubwayStationName, school_x=schoolCorX, school_y=schoolCorY, hubway_x=hubwayCorX, hubway_y=hubwayCorY)
+		
+
+
+@app.route("/showTwoResult", methods=["GET","POST"])
+def showTwoResult():
+	if(request.method == 'POST'):
+		schoolNameInput = request.form.get('schoolNameInput')
+		client = dml.pymongo.MongoClient()
+		repo = client.repo
+		repo.authenticate('debhe_shizhan0_wangdayu_xt', 'debhe_shizhan0_wangdayu_xt')
+		newStationList = repo['debhe_shizhan0_wangdayu_xt.newSchoolSubDis'].find()
+		for row in newStationList:
+			if row['schoolName'] == schoolNameInput:
+				subwayName = row['subwayStation']
+				schoolCorX = row['schoolX']
+				schoolCorY = row['schoolY']
+				subwayCorX = findSubwayX(subwayName)
+				subwayCorY = findSubwayY(subwayName)
+		oldStationList = repo['debhe_shizhan0_wangdayu_xt.schoolHubwayDistance'].find()
+		for row in oldStationList:
+			if row['schoolName'] == schoolNameInput:
+				hubwayCorX = row['Hubway_Cor_x']
+				hubwayCorY = row['Hubway_Cor_y']
+				hubwayStationName = row['hubwayStation']
+		schoolName = schoolNameInput
+		repo.logout()
+		return render_template("showTwoResult.html", schoolName = schoolName, subwayStationName = subwayName, school_x = schoolCorX, school_y = schoolCorY, subway_x= subwayCorX, subway_y = subwayCorY,hubwayStationName = hubwayStationName, hubway_x=hubwayCorX, hubway_y=hubwayCorY )
+	return render_template("showTwoResult.html")
+
+
 def findSubwayX(Name):
 	client = dml.pymongo.MongoClient()
 	repo = client.repo
@@ -156,14 +202,6 @@ def schoolAllHubway():
 		return render_template("schoolAllHubway.html", schoolName = schoolNameInput, school_x = s_x, school_y = s_y, hubways = hubwayList)
 	repo.logout()
 	return render_template("schoolAllHubway.html", hubways = hubwayList)
-
-
-
-
-
-
-
-
 
 if __name__ == '__main__':
     app.run(debug=True)

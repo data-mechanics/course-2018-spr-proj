fefd8becdfa374667fc04eb2748d125c08c84ca4
dwang1995 <wangdayuan1995@gmail.com>
2018-04-25 00:46:26 -0400

    add some new functions

--- debhe_shizhan0_wangdayu_xt/project3/app.py
+++ debhe_shizhan0_wangdayu_xt/project3/app.py
@@ -24,6 +24,7 @@ def schoolHubwayStation():
 		repo.authenticate('debhe_shizhan0_wangdayu_xt', 'debhe_shizhan0_wangdayu_xt')
 		schoolHubwayList = []
 		schoolHubwayList = repo['debhe_shizhan0_wangdayu_xt.schoolHubwayDistance'].find()
+		find = False
 	#schoolHubwayList = list(mongo.db.debhe_shizhan0_wangdayu_xt.schoolHubwayDistance.find())
 		for row in schoolHubwayList:
 			if row['schoolName'] == schoolNameInput:
@@ -33,9 +34,74 @@ def schoolHubwayStation():
 				schoolCorY = row['School_Cor_y']
 				hubwayCorX = row['Hubway_Cor_x']
 				hubwayCorY = row['Hubway_Cor_y']
+				find = True
 		repo.logout()
-		return render_template("schoolHubwayStation.html", schoolName = schoolName, hubwayStationName = hubwayStationName, school_x=schoolCorX, school_y=schoolCorY, hubway_x=hubwayCorX, hubway_y=hubwayCorY)
+		if(find == True):
+			return render_template("schoolHubwayStation.html", schoolName = schoolName, hubwayStationName = hubwayStationName, school_x=schoolCorX, school_y=schoolCorY, hubway_x=hubwayCorX, hubway_y=hubwayCorY)
+		else:
+			return render_template("schoolHubwayStation.html", message = "School Not Found")
 	return render_template("schoolHubwayStation.html")
+
+@app.route("/schoolNewBikeHub", methods=["GET","POST"])
+def schoolNewBikeHub():
+	if(request.method == 'POST'):
+		schoolNameInput = request.form.get('schoolNameInput')
+		client = dml.pymongo.MongoClient()
+		repo = client.repo
+		repo.authenticate('debhe_shizhan0_wangdayu_xt', 'debhe_shizhan0_wangdayu_xt')
+		schoolList = []
+		schoolList = repo['debhe_shizhan0_wangdayu_xt.newSchoolSubDis'].find()
+		for row in schoolList:
+			if row['schoolName'] == schoolNameInput:
+				schoolName = row['schoolName']
+				subwayName = row['subwayStation']
+				schoolCorX = row['schoolX']
+				schoolCorY = row['schoolY']
+				subwayCorX = findSubwayX(subwayName)
+				subwayCorY = findSubwayY(subwayName)
+		repo.logout()
+		return render_template("schoolNewBikeHub.html", schoolName = schoolName, subwayStationName = subwayName, school_x=schoolCorX, school_y=schoolCorY, subway_x=subwayCorX, subway_y=subwayCorY)
+	return render_template("schoolNewBikeHub.html")
+
+def findSubwayX(Name):
+	client = dml.pymongo.MongoClient()
+	repo = client.repo
+	repo.authenticate('debhe_shizhan0_wangdayu_xt', 'debhe_shizhan0_wangdayu_xt')
+	subwayList = repo['debhe_shizhan0_wangdayu_xt.subwayStop'].find()
+	for row in subwayList:
+		if( row['stopName'] == Name ):
+			repo.logout()
+			return row['X']
+	repo.logout()
+	return "Not Found"
+
+def findSubwayY(Name):
+	client = dml.pymongo.MongoClient()
+	repo = client.repo
+	repo.authenticate('debhe_shizhan0_wangdayu_xt', 'debhe_shizhan0_wangdayu_xt')
+	subwayList = repo['debhe_shizhan0_wangdayu_xt.subwayStop'].find()
+	for row in subwayList:
+		if( row['stopName'] == Name ):
+			repo.logout()
+			return row['Y']
+	repo.logout()
+	return "Not Found"
+
+@app.route("/checkAllSchools", methods=["GET"])
+def checkAllSchools():
+	client = dml.pymongo.MongoClient()
+	repo = client.repo
+	repo.authenticate('debhe_shizhan0_wangdayu_xt', 'debhe_shizhan0_wangdayu_xt')
+	schoolList = []
+	schoolList = repo['debhe_shizhan0_wangdayu_xt.schoolHubwayDistance'].find()
+	returnList = []
+	for row in schoolList:
+		s_n = row['schoolName']
+		s_x = row['School_Cor_x']
+		s_y = row['School_Cor_y']
+		returnList.append([s_n, s_x, s_y])
+	return render_template("checkAllSchools.html", schools = returnList)
+
 
 if __name__ == '__main__':
     app.run(debug=True)

b669c2463e38054a89729e8377cb4c4462ce942b
Chanan Suksangium <csuksan@bu.edu>
2018-04-26 21:03:26 -0400

    added marker feature for returned restaurants

--- bstc_csuksan_semina_tedkong/web_app/app/static/index.js
+++ bstc_csuksan_semina_tedkong/web_app/app/static/index.js
@@ -2,10 +2,19 @@
 var rNames = document.getElementById("restaurantNames");
 var lat,lon;
 var map = document.getElementById("map");
-
+var layerId = 0;
 var retryCnt = 0;
-
-
+//global var layer group
+var layerGroup = L.layerGroup()
+//new icon specs to differ from user location
+var greenIcon = new L.Icon({
+  iconUrl: 'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-green.png',
+  shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41]
+});
 
 requestPermission();
 
@@ -74,14 +83,33 @@ function submit() {
     let yelpscale = document.getElementById("yelp_scale").value;
     let violationscale = document.getElementById("violation_scale").value;
     let num = document.getElementById("num_restaurant").value;
-    // console.log(yelpscale,violationscale,lat,lon,num);
+    //console.log(yelpscale,violationscale,lat,lon,num);
     $.get("/receivedata", {"yelp": yelpscale, 
       "violation": violationscale,
       "lat": lat,
       "lon": lon,
       "rest":num
-    }).done(function(data){console.log(data)});
-    // , function (res) {
-    //   updateMarkers
-    // })
+    }).done(function(data){
+      var t = JSON.parse(data);
+            //check if markers already exist on map
+      if(map.hasLayer(layerGroup)) {
+          map.removeLayer(layerGroup);
+          layerGroup.clearLayers();
+        } 
+      //var x as iterator
+      var x;
+      //adding marker to layerGroup
+      for (x in t['restaurants']) {
+        console.log(t['restaurants'][x]);
+        let latInfo = t['restaurants'][x][1];
+        let longInfo = t['restaurants'][x][2];
+        let restaurantInfo = t['restaurants'][x][0]+"<dd>"+"Health Violation Score: " + t['restaurants'][x][3] 
+        + "<dd>"+ "Yelp Rating: "+ t['restaurants'][x][4];
+        var tempMarker = L.marker([latInfo, longInfo], {icon: greenIcon}).bindPopup(restaurantInfo);
+        layerGroup.addLayer(tempMarker);
+      }
+      //add layer group to map
+      layerGroup.addTo(map);
+      });
+    
 }

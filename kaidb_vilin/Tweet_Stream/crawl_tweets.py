4b9d767da056e7e38f8af1d2703961b9883a1c4b
Vilin97 <vasilin97@gmail.com>
2018-04-28 18:08:38 -0400

    added live streaming by countries and by states. Heatmap and plot test do not work, they are just examples to be modified

--- kaidb_vilin/Tweet_Stream/crawl_tweets.py
+++ kaidb_vilin/Tweet_Stream/crawl_tweets.py
@@ -1,6 +1,6 @@
 # This is the primary file for crawling tweets via twitters tweetpy api
 
-# Imports. Tweetpy required. 
+# Imports. Tweetpy required.
 from tweepy import (Stream, OAuthHandler)
 from tweepy.streaming import StreamListener
 import pandas as pd
@@ -11,22 +11,22 @@ import sys
 
 class DataLog:
     """
-    Param class 
+    Param class
     Detects duplicate tweets
     """
     unique_users = {}
     unique_tweets = {}
     save_path =  "./TWEETS/"
     fetched_tweets_filename = "tweet_stream.csv"
-    # Crawl Count 
+    # Crawl Count
     CC = 0
 
-    
+
 
 
 class Listener(StreamListener):
     #counter
-    tweet_counter = 0 
+    tweet_counter = 0
 
     def login(self):
         # 2 levels out
@@ -47,10 +47,10 @@ class Listener(StreamListener):
         tmp_df = pd.DataFrame([status._json])
         # Retrieve tweet it
         tmp_tweet_id = tmp_df.id.values[0]
-            
+
         # Get the unique identifier for the user
         tmp_user = tmp_df.user.values[0]['id']
-        # log instances of new users 
+        # log instances of new users
         if tmp_user not in DataLog.unique_users:
             DataLog.unique_users[tmp_user] = True
 
@@ -58,12 +58,12 @@ class Listener(StreamListener):
         if tmp_tweet_id not in DataLog.unique_tweets:
             DataLog.unique_tweets[tmp_tweet_id] = True
             save_name =  str("user_{}_tweetID_{}_".format(tmp_user, tmp_tweet_id)) + DataLog.fetched_tweets_filename
-            # Write by 
+            # Write by
             tmp_df.to_csv(DataLog.save_path + save_name)
             print("succesfully wrote  tweet {}".format(tmp_tweet_id))
         DataLog.CC +=1
         print("Crawl index:{}".format(DataLog.CC))
-        
+
         if Listener.tweet_counter < Listener.stop_at:
             return True
         else:
@@ -94,10 +94,6 @@ print("Crawl Begining")
 listener = Listener()
 # bounding box for boston: -71.1912, 42.2279, -70.8085, 42.3973
 
-listener.getTweetsByGPS(np.inf, -71.1912, 42.2279, -70.8085, 42.3973) 
+listener.getTweetsByGPS(np.inf, -71.1912, 42.2279, -70.8085, 42.3973)
 
 print(DataLog.unique_users, DataLog.unique_tweets)
-
-
-
-

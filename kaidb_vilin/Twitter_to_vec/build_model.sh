7486b6caa8551ae3b7d91a404e2864479d814f4d
Kai Bernardini <kbernardini@KMBP.local>
2018-05-06 23:19:58 -0400

    context problem fixed

---
+++ kaidb_vilin/Twitter_to_vec/build_model.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# this is the series of executions used to build a Logistic regression model for Classification
+# Execution should be with python 3
+
+
+echo "Downloading Labled Twitter Sentiment Data"
+echo "All data will be stemmed, and stored as a pickle object"
+
+cd Twitter_to_vec
+python  get_clean_twitter_sentiment.py
+echo "Building the Model "
+python sentiment_model_l2_LR.py
+echo "DONE"
